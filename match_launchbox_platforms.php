4ec086a51f3e8d3983430e1dab58773c1e3c86d7
Joe Huss <detain@interserver.net>
2019-06-30 01:36:33 -0400

    Loads all the LaunchBox Platforms from its XML Data files, and looks through all your rom directories and uses some strong comparison logic to calculate how close the launchbox platform names is to the roms platform names and match up the closest ones.
    It then updates the LaunchBox Platforms XMl setting the Games/Roms folder path for each
    platform to the appropriate directory.

--- match_launchbox_platforms.php
+++ match_launchbox_platforms.php
@@ -1,19 +1,28 @@
 <?php
-
+/**
+* Loads all the LaunchBox Platforms from its XML Data files, and looks through all your
+* rom directories and uses some strong comparison logic to calculate how close the launchbox
+* platform names is to the roms platform names and match up the closest ones.  
+* 
+* It then updates the LaunchBox Platforms XMl setting the Games/Roms folder path for each
+* platform to the appropriate directory. 
+*/
 require_once __DIR__.'/StringCompare.php';
 
 $pathGlob = '/storage/vault*/roms/*/*';
 
 $xmlFile = '/storage/Emulators/LaunchBox/Data/Platforms.xml';
 $xmlString = file_get_contents($xmlFile);
-preg_match_all('/<Platform>.*<Name>(.*)<\/Name>.*<\/Platform>/msuU', $xmlString, $matches);
+preg_match_all('/<Platform>.*<Name>(.*)<\/Name>.*(<Folder.[^>]*>).*<\/Platform>/msuU', $xmlString, $matches);
 $platforms = $matches[1];
-$platformCount = count($matches[1]);
+$folders = $matches[2];
+$platformCount = count($matches[1]);               
 echo "Parsed out {$platformCount} platforms from {$xmlFile}\n";
 $pathPlatforms = [];
 foreach (glob($pathGlob) as $path) {
 	if (is_dir($path)) {
         $pathPlatforms[] = [
+            'path' => $path,
             'company' => basename(dirname($path)),
             'platform' => basename($path)
         ];
@@ -21,12 +30,13 @@ foreach (glob($pathGlob) as $path) {
 }
 $pathPlatformCount = count($pathPlatforms);
 echo "Parsed out {$pathPlatformCount} platforms from {$pathGlob}\n";
-$matches = [];
-foreach ($platforms as $platform) {
-    $matches[$platform] = [];
+$allMatches = [];
+foreach ($platforms as $platformIdx => $platform) {
+    $allMatches[$platform] = [];
     $platformMatches = [];
     $platformPercentMatches = [];
     foreach ($pathPlatforms as $pathPlatformData) {
+        $pathPath = $pathPlatformData['path'];
         $pathCompany  = $pathPlatformData['company'];
         $pathPlatform  = $pathPlatformData['platform'];
         $pathPlatformArray = [
@@ -50,7 +60,7 @@ foreach ($platforms as $platform) {
             $platformMatches[$pathPlatform] = $matchPercent;
             if (!isset($platformPercentMatches[$matchPercent]))
                 $platformPercentMatches[$matchPercent] = [];
-            $platformPercentMatches[$matchPercent][] = [$pathCompany, $pathPlatformString, $pathPlatform, $diffPercent];
+            $platformPercentMatches[$matchPercent][] = [$pathCompany, $pathPlatformString, $pathPlatform, $diffPercent, $pathPath];
         }
     }
     $percents = array_keys($platformPercentMatches);
@@ -58,9 +68,16 @@ foreach ($platforms as $platform) {
     foreach ($percents as $idx => $percent) {
         //echo "Platform {$platform} checking percent {$percent} results\n";
         foreach ($platformPercentMatches[$percent] as $platformPercentIdx => $pathPlatformData) {
-            list($pathCompany, $pathPlatformString, $pathPlatform, $diffPercent) = $pathPlatformData;                          
-            echo sprintf("%47s %6.2f %6.2f %40s %47s %47s\n", $platform, $percent, $diffPercent, $pathCompany, $pathPlatform, $pathPlatformString);
+            list($pathCompany, $pathPlatformString, $pathPlatform, $diffPercent, $pathPath) = $pathPlatformData;                          
+            echo sprintf("%50s %10.2f %10.2f %-60s\n", $platform, $percent, $diffPercent, $pathPath);
+            $platformString = $matches[0][$platformIdx];
+            $platformFolderString = $matches[2][$platformIdx];
+            $platformNewFolderString = '<Folder>'.str_replace(['&', '/storage/','/'], ['&amp;', 'Y:\\', '\\'],$pathPath).'</Folder>';
+            $platformNewString = str_replace($platformFolderString, $platformNewFolderString, $platformString);
+            $xmlString = str_replace($platformString, $platformNewString, $xmlString);
+
             break 2;
         }
     }    
 }
+file_put_contents($xmlFile.'.new', $xmlString);
