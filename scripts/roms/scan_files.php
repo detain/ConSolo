95304fdea30850a94002899f2216921bff5483ba
Joe Huss <detain@interserver.net>
2019-07-13 01:36:15 -0400

    work on the compressed files handling getting it mostly working

--- scripts/roms/scan_files.php
+++ scripts/roms/scan_files.php
@@ -2,42 +2,112 @@
 /**
 * Scans through an array of path globs finding all files and scanning them performing
 * checksumming, and storing of basic information.  This process can take a long time on
-* a larger set of files so there is an automated timed backup that by default occurs 
-* roughly every 60 seconds.
-* 
-* The backup interval and the array of paths globs can be configured in the lines
-* directly below.  
+* a larger set of files.  It also scans inside compressed files. 
 */
 include __DIR__.'/../../vendor/autoload.php';
 
-function hasFileExt($file, $ext) {
-    $ext = substr($ext, 0, 1) == '.' ? $ext : '.'.$ext);
-    return (substr($file, 0 - strlen($ext)) == $ext);
-}
-
-function loadFiles($path = null) {
+function updateCompressedFile($path, $parentId)  {
     /**
     * @var \Workerman\MySQL\Connection
     */
     global $db;
-    global $files, $paths;
-    $files = [];
-    $paths = [];
-    if (is_null($path)) {
-        $tempFiles = $db->query("select * from files where parent is null");
+    global $files, $tmpDir;
+    $hashAlgos = ['md5', 'sha1', 'crc32']; // use hash_algos() to get all possible hashes
+    //$hashAlgos = ['md5', 'crc32']; // use hash_algos() to get all possible hashes
+    //$hashAlgos = ['md5']; // use hash_algos() to get all possible hashes
+    $statFields = ['size', 'mtime']; // fields are dev,ino,mode,nlink,uid,gid,rdev,size,atime,mtime,ctime,blksize,blocks
+    $parentData = $files[$parentId];
+    $parentPath = $parentData['path'];
+    $virtualPath = substr($tmpDir, '', $path);
+    $linkedPath = substr($tmpDir, $parentPath.'#', $path); 
+    $realPath = $path;
+    $pathStat = stat($realPath);
+        $fileData = [];
+    $newData = [];
+    foreach ($statFields as $statField) {
+        if (!isset($fileData[$statField]) || $pathStat[$statField] != $fileData[$statField]) {
+            $newData[$statField] = $pathStat[$statField];            
+        }
+        $fileData[$statField] = $pathStat[$statField];
+    }
+    foreach ($hashAlgos as $hashAlgo) {
+        if (!isset($fileData[$hashAlgo])) {
+            $newData[$hashAlgo] = hash_file($hashAlgo, $path);
+            $fileData[$hashAlgo] = hash_file($hashAlgo, $path);
+        }
+    }
+    $fileData['path'] = $virtualPath;
+    $fileData['parent'] = $parentId;
+    $id = $db->insert('files')->cols($fileData)->query();
+    echo "  Added file #{$id} {$virtualPath} from Compressed parent {$parentData['path']}\n";
+}
+
+function updateCompressedDir($path, $parentId) {
+    global $files, $skipGlobs;
+    //echo "  Added directory {$path}\n";
+    foreach (glob($path.'/*') as $subPath) {
+        $bad = false;
+        foreach ($skipGlobs as $skipGlob) {
+            if (substr($subPath, 0, strlen($skipGlob)) == $skipGlob) {
+                echo 'Skipping Path '.$subPath.' matching glob '.$skipGlob.PHP_EOL;
+                $bad = true;
+            }
+        }
+        if ($bad === false) {
+            if (is_dir($subPath)) {
+                updateCompressedDir($subPath, $parentId);
+            } else {
+                updateCompressedFile($subPath, $parentId);
+            }
+        }
+    }    
+}
+
+function cleanTmpDir() {
+    global $tmpDir;
+    echo 'Cleaning Temp Dir '.$tmpDir.PHP_EOL;
+    passthru('rm -rf '.$tmpDir);
+}
+
+function extractCompressedFile($path, $compressionType) {
+    global $tmpDir;
+    cleanTmpDir();
+    mkdir($tmpDir);
+    $escapedFile = escapeshellarg($path);
+    passthru('7z x -o'.$tmpDir.' '.escapeshellarg($path), $return);
+    return ($return == 0);
+}
+
+function hasFileExt($file, $ext) {
+    $origExt = $ext;
+    $ext = substr($ext, 0, 1) == '.' ? $ext : '.'.$ext;
+    if (substr($file, 0 - strlen($ext)) == $ext) {
+        return true;
     } else {
-        $tempFiles = $db->query("select * from files where path like '{$path}%' and parent is null");
+        return false;
     }
-    echo '[Line '.__LINE__.'] Current Memory Usage: '.memory_get_usage().PHP_EOL;
-    foreach ($tempFiles as $idx => $data) {
-        $id = $data['id'];
-        unset($data['id']);
-        unset($data['parent']);
-        $files[$id] = $data;
-        $paths[$data['path']] = $id;
-        unset($tempFiles[$idx]);        
+}
+
+function compressedFileHandler($path) {
+    /**
+    * @var \Workerman\MySQL\Connection
+    */
+    global $db;
+    global $files, $paths, $compressionTypes, $tmpDir;
+    foreach ($compressionTypes as $idx => $compressionType) {
+        if (hasFileExt($path, $compressionType)) {
+            // handle compressed file
+            $parentId = $paths[$path];
+            echo 'Found Compressed file #'.$parentId.' '.$path.' of type '.$compressionType.PHP_EOL;
+            $rows = $db->query("select * from files where parent={$parentId}");
+            if (count($rows) == 0) {
+                if (extractCompressedFile($path, $compressionType)) {
+                    updateCompressedDir($tmpDir, $parentId);
+                }                
+                cleanTmpDir();
+            }
+        }
     }
-    echo '[Line '.__LINE__.'] Current Memory Usage: '.memory_get_usage().PHP_EOL;
 }
 
 function updateFile($path)  {
@@ -45,7 +115,7 @@ function updateFile($path)  {
     * @var \Workerman\MySQL\Connection
     */
     global $db;
-    global $files, $paths, $compressionTypes;
+    global $files, $paths;
     //$hashAlgos = ['md5', 'sha1', 'crc32']; // use hash_algos() to get all possible hashes
     //$hashAlgos = ['md5', 'crc32']; // use hash_algos() to get all possible hashes
     $hashAlgos = ['md5']; // use hash_algos() to get all possible hashes
@@ -78,6 +148,7 @@ function updateFile($path)  {
         //echo "  Skipping {$path}, Its Already Hashed and Still The Same Size and Modification Time\n";
         //echo " Skipping {$path}\n";
         //echo '-';
+        compressedFileHandler($path);
         return;
     }
     if (!isset($paths[$path])) {
@@ -91,11 +162,7 @@ function updateFile($path)  {
         echo "  Updated file #{$paths[$path]} {$path}\n".var_export($newData, true).PHP_EOL;
     }
     $files[$id] = $fileData;
-    foreach ($compressionTypes as $compressionType) {
-        if (hasFileExt($path, $compressionType)) {
-            // handle compressed file
-        }
-    }
+    compressedFileHandler($path);
 }
 
 function updateDir($path) {
@@ -119,10 +186,39 @@ function updateDir($path) {
     }    
 }
 
+function loadFiles($path = null) {
+    /**
+    * @var \Workerman\MySQL\Connection
+    */
+    global $db;
+    global $files, $paths;
+    $files = [];
+    $paths = [];
+    if (is_null($path)) {
+        $tempFiles = $db->query("select * from files where parent is null");
+    } else {
+        $tempFiles = $db->query("select * from files where path like '{$path}%' and parent is null");
+    }
+    echo '[Line '.__LINE__.'] Current Memory Usage: '.memory_get_usage().PHP_EOL;
+    foreach ($tempFiles as $idx => $data) {
+        $id = $data['id'];
+        unset($data['id']);
+        unset($data['parent']);
+        $files[$id] = $data;
+        $paths[$data['path']] = $id;
+        unset($tempFiles[$idx]);        
+    }
+    echo '[Line '.__LINE__.'] Current Memory Usage: '.memory_get_usage().PHP_EOL;
+}
+
+
 $pathGlobs = ['/storage/vault*/roms'];
+$pathGlobs = ['/storage/vault*/roms/*'];
+$pathGlobs = ['/storage/vault*/roms/Inf*'];
 $skipGlobs = ['/storage/vault6/roms/toseciso/'];
-global $files, $db, $paths, $skipGlobs, $compressionTypes;
+$tmpDir = '/tmp/scanfiles';
 $compressionTypes = ['7z', 'rar', 'zip'];
+global $files, $db, $paths, $skipGlobs, $compressionTypes, $tmpDir;
 $db = new Workerman\MySQL\Connection('127.0.0.1', '3306', 'consolo', 'consolo', 'consolo');
 foreach ($pathGlobs as $pathGlob) {
     foreach (glob($pathGlob) as $path) {
