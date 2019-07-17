c8b0c65a0167af428dbb406889861ae73f4c5853
Joe Huss <detain@interserver.net>
2019-07-16 22:08:55 -0400

    some work on the no-intro import

--- scripts/no-intro/update.php
+++ scripts/no-intro/update.php
@@ -73,15 +73,17 @@ if (count($row) == 0) {
 }
 $version = trim(`curl -s "https://datomatic.no-intro.org/?page=download&fun=daily"|grep "Version <b>"|cut -d">" -f2-|cut -d" " -f1`);
 echo "Last:    {$last}\nCurrent: {$version}\n";
-if (intval($version) <= intval($last)) {
+if (intval(str_replace('-','', $version)) <= intval(str_replace('-','', $last))) {
     die('Already Up-To-Date'.PHP_EOL);
 }
+$dir = '/storage/data/dat/No-Intro/Standard';
+$glob = '/storage/data/dat/No-Intro/*/*';
 echo `curl -s "https://datomatic.no-intro.org/?page=download&fun=daily" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -H "Accept-Language: en-US,en;q=0.5" --compressed -H "Referer: https://datomatic.no-intro.org/?page=download&fun=daily" -H "Content-Type: application/x-www-form-urlencoded" -H "Connection: keep-alive" -H "Cookie: PHPSESSID=bMGvdze90A"%"2CQPGKo64uTP2" -H "Upgrade-Insecure-Requests: 1" --data "dat_type=standard&download=Download" -o nointro.zip`;
-echo `rm -rf /tmp/update;`;
-echo `7z x -o/tmp/update nointro.zip;`;
+echo `rm -rf {$dir};`;
+echo `7z x -o{$dir} nointro.zip;`;
 unlink('nointro.zip');
 $db->query('truncate nointro_roms');
-foreach (glob('/tmp/update/*') as $xmlFile) {
+foreach (glob($glob) as $xmlFile) {
     $list = basename($xmlFile, '.dat');
 	//$list = str_replace([' ','-'],['_','_'], strtolower($list));
 	echo "Getting {$list} List   ";
@@ -128,5 +130,5 @@ foreach (glob('/tmp/update/*') as $xmlFile) {
         echo "no games, skipping\n";
     }
 }
-echo `rm -rf /tmp/update;`;
+//echo `rm -rf /tmp/update;`;
 $db->query("update config set config.value='{$version}' where config.key='{$configKey}'"); 
