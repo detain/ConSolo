74e1e1161b7a3ca2261c3166243ef094e1cd8093
Joe Huss <detain@interserver.net>
2019-07-31 04:54:09 -0400

    updated script redoing the way it does matching

--- src/Matching/AllPlatforms.php
+++ src/Matching/AllPlatforms.php
@@ -13,169 +13,211 @@ use FuzzyWuzzy\Process;
 */
 global $db;
 
-$useAlternates = false;
-$alternates = [];
+$glob = '/storage/vault*/roms/*/*';
+$platforms = [];
+$name2id = [];
+$path2id = [];
+$good = [];
+$missing = [];
+echo 'Loading Platforms'.PHP_EOL;
+$rows = $db->query("select * from platforms");
+foreach ($rows as $idx => $row) {
+    $platforms[$row['id']] = $row;
+    $name2id[$row['name']] = $row['id'];
+}
+
+echo 'Loading Platform Matchs with all sources'.PHP_EOL;
+$rows = $db->query("select * from platform_matches");
+foreach ($rows as $idx => $row) {
+    $name2id[$row['name']] = $row['platform_id'];
+}
+            
+echo 'Loading LaunchBox Alternative Platforms'.PHP_EOL;
+$rows = $db->query("select Name as name, Alternate as alternate from launchbox_platformalternatenames");
+foreach ($rows as $idx => $row) {
+    if (!isset($name2id[$row['name']])) {
+        echo 'Error finding '.$row['name'].PHP_EOL;
+    } else {
+        $name2id[$row['alternate']] = $name2id[$row['name']];
+    }
+}
+
+echo 'Mapping Developer and Manufacturer Platform Names'.PHP_EOL;
+foreach ($name2id as $name => $id) {
+    if (isset($platforms[$id])) {
+        foreach (['developer','manufacturer'] as $field) {
+            if (!is_null($platforms[$id][$field])) {
+                $name2id[$platforms[$id][$field].' '.$name] = $id;
+                $name2id[$platforms[$id][$field].' - '.$name] = $id;
+                if (strpos($name, $platforms[$id][$field].' - ') !== false) {
+                    $name = str_replace($platforms[$id][$field].' - ', '', $name);
+                    $name2id[$name] = $id;
+                    $name2id[$platforms[$id][$field].' '.$name] = $id;
+                }
+            }
+        }
+    }
+}
+
+echo 'Setting up Searching'.PHP_EOL;
+$maxResults = 10;
 $names = [];
 $namesAssoc = [];
-$lower = [];
-if ($useAlternates == true) {    
-    $rows = $db->query("select Name as name, Alternate as alternate from launchbox_platformalternatenames");
-    foreach ($rows as $idx => $row) {
-        $alternates[strtolower($row['alternate'])] = $row['name'];
-        $lower[strtolower($row['alternate'])] = $row['alternate'];
-    }
-}
-foreach (glob(__DIR__.'/*/match.php') as $file) {
-    $lines = explode("\n", trim(`php {$file}`));
-    $lines = array_unique($lines);
-    foreach ($lines as $line) {
-        $words = explode(' ', $line);
-        if (isset($words[1]) && $words[0] == $words[1]) {
-            $line = str_replace($words[0].' '.$words[1], $words[0], $line);
-        }
-        $lower[strtolower($line)] = $line;
-        $realLine = $line;
-        $line = strtolower($line);
-        if (array_key_exists($line, $alternates)) {
-            $line = $alternates[$line];
-        }
-        if (!in_array($line, $names)) {
-            if (!isset($names[$line]))
-                $names[$line] = [];
-            if (!isset($names[$line][$realLine]))
-                $names[$line][$realLine] = [];
-            $names[$line][$realLine][] = basename(dirname($file));
-            $namesAssoc[] = ['name' => $line];
-        }
-    }
-}
-$lines = [];
-$keys = array_keys($names);
-sort($keys);
-reset($keys);
-foreach ($keys as $idx) {
-    $data = $names[$idx];
-    $parts = [];
-    foreach ($data as $file => $types) {
-        $parts[] = '"'.$file.'": ["'.implode('", "', $types).'"]';         
-    }
-    $lines[] = '"'.(isset($lower[$idx]) ? $lower[$idx] : $idx).'": { '.implode(', ', $parts).' }'; 
-}
-echo '{'.PHP_EOL.'    '.implode(','.PHP_EOL.'    ', $lines).PHP_EOL.'}'.PHP_EOL;
-exit;
-$rows = $db->query("select name from launchbox_platforms");
-foreach ($rows as $idx => $row) {
-    $description = $row['name'];
-    $names[] = $description;
+foreach ($name2id as $name => $id) {
+    $names[] = $name;
     $namesAssoc[] = [
-        'name' => $description
+        'name' => $name
     ];
-    $rows[$idx]['name'] = $description;
 }
 $fuse = new \Fuse\Fuse($namesAssoc, ['keys' => ['name']]);
 $fuzz = new Fuzz();
 $process = new Process($fuzz);
-$rows = $db->query("select platform_description from mame_software group by platform_description");
-$maxResults = 10;
-$done = [];
-foreach ($rows as $idx => $row) {
-    $scores = [];
-    $description = $row['platform_description'];
-    foreach ($mediaTypes as $type) {
-        $description = preg_replace('/ *'.preg_quote($type, '/').'$/i', '', $description);
-    }
-    if (in_array($description, $done)) {
-        continue;
-    }
-    echo $description."\n";
-    $done[] = $description;
-    $out = [];
-    $results = $fuse->search($description); 
-    foreach ($results as $idx => $data) {
-        if ($useAlternates == true && isset($alternates[$data['name']])) {
-            $out[] = $data['name'] .' ('.$alternates[$data['name']].')';
-        } else {
-            $out[] = $data['name'];
+
+echo 'Mapping File Paths'.PHP_EOL;
+foreach (glob($glob) as $path) {
+    if (is_dir($path)) {
+        $name = basename($path);
+        if (strpos($name, ' (') !== false) {
+            $name = substr($name, 0, strpos($name, ' ('));
         }
-        $scores[$data['name']] = (isset($scores[$data['name']]) ? $scores[$data['name']] : 0) + ceil(100 - ($idx * (100 / count($results))));
-    }
-    if (count($out) > 0) {
-        echo '      Fuse:'.implode(', ', $out).PHP_EOL;
-    }
-    $c = $process->extract($description, $names);
-    $results = $c->toArray();
-    $out = [];
-    foreach ($results as $idx => $data) {
-        if ($useAlternates == true && isset($alternates[$data[0]])) {
-            $out[] = $data[0].' ('.$alternates[$data[0]].')'.' ('.$data[1].'%)';
-        } else {
-            $out[] = $data[0].' ('.$data[1].'%)';
+        $developer = basename(dirname($path));
+        $found = false;
+        $names = [$name, $developer.' '.$name, $developer.' - '.$name];
+        if (strpos($name, $developer.' - ') !== false) {
+            $name = str_replace($developer.' - ', '', $name);
         }
-        $scores[$data[0]] = (isset($scores[$data[0]]) ? $scores[$data[0]] : 0) + $data[1];
-        if ($idx >= $maxResults) {
-            break;
-        }
-    }
-    if (count($out) > 0) {
-        echo '      Fuzzy:'.implode(', ', $out).PHP_EOL;
-    }
-    $levs = [];
-    foreach ($names as $name) {
-        $levs[$name] = levenshtein($description, $name);
-        //$scores[$name] = (isset($scores[$name]) ? $scores[$name] : 0) + (100 - ($levs[$name] * 15));
-    }
-    $levValues = array_values($levs);
-    $levValues = array_unique($levValues);
-    sort($levValues);
-    reset($levValues);
-    $found = 0;
-    $foundResults = 0;
-    $out = [];
-    foreach ($levValues as $levValue) {
-        $found++;
-        foreach ($levs as $name => $value) {
-            if ($value == $levValue) {
-                $foundResults++;
-                if ($useAlternates == true && isset($alternates[$name])) {
-                    $out[] = $name.' ('.$alternates[$name].')'.' ('.$value.')';
-                } else {
-                    $out[] = $name.' ('.$value.')';
-                }
-                $scores[$name] = (isset($scores[$name]) ? $scores[$name] : 0) + (100 - ($found * 10 + $value)); 
-                if ($foundResults >= $maxResults) {
-                    break;
-                }
+        foreach ($names as $search) {
+            if (array_key_exists($search, $name2id)) {
+                $found = true;
+                $path2id[$path] = $name2id[$search];
+                if (!in_array($developer.'/'.$name, $good))
+                    $good[] = $developer.'/'.$name;
+                break;
             }
         }
-        if ($found >= 5 || $foundResults >= $maxResults) {
-            break;
+        if ($found == false) {
+            if (!in_array($developer.'/'.$name, $missing))
+                $missing[] = $developer.'/'.$name;
         }
     }
-    echo '      Levenshtein:'.implode(', ', $out).PHP_EOL;
-    $rows[$idx]['platform_description'] = $description;
-    /*
-    $scoreValues = array_values($scores);
-    $scoreValues = array_unique($scoreValues);
-    rsort($scoreValues);
-    reset($scoreValues);
-    $found = 0;
-    $foundResults = 0;
-    $out = [];
-    foreach ($scoreValues as $scoreValue) {
-        $found++;
-        foreach ($scores as $name => $value) {
-            if ($value == $scoreValue) {
-                $foundResults++;
-                $out[] = $name.' ('.$value.')';
-                if ($foundResults >= $maxResults) {
-                    break;
-                }
-            }
-        }
-        if ($found >= 5 || $foundResults >= $maxResults) {
-            break;
-        }
-    }
-    echo '      Overall:'.implode(', ', $out).PHP_EOL;
-    */
 }
+$done = [];
+echo 'Found:'.PHP_EOL;
+foreach ($path2id as $path => $id) {
+    $platform = $platforms[$id];
+    $name = basename($path);
+    $developer = basename(dirname($path));
+    echo '    '.$developer.'/'.$name.'  =>  '.$platform['name'].PHP_EOL;
+}
+echo 'Missing:'.json_encode($missing, JSON_PRETTY_PRINT).PHP_EOL;
+echo 'Found '.count($path2id).' Missing '.count($missing).PHP_EOL;
+
+foreach ($missing as $missingData) {
+    list($developer, $name) = explode('/', $missingData);
+    //$search = $name;
+    $searches = [$name];
+    if (!in_array(str_replace($developer.' '.$developer, $developer, $developer.' '.$name), $searches)) {
+        $searches[] = str_replace($developer.' '.$developer, $developer, $developer.' '.$name); 
+    }
+    if (!in_array(str_replace($developer.' - '.$developer, $developer, $developer.' - '.$name), $searches)) {
+        $searches[] = str_replace($developer.' - '.$developer, $developer, $developer.' - '.$name); 
+    }
+    foreach ($searches as $search) {
+        $scores = [];
+        echo 'Searching for '.$search.PHP_EOL;
+        
+        $results = $fuse->search($search); 
+        foreach ($results as $idx => $data) {
+            $out[] = $data['name'];
+            $scores[$data['name']] = (isset($scores[$data['name']]) ? $scores[$data['name']] : 0) + ceil(100 - ($idx * (100 / count($results))));
+            if ($idx >= $maxResults) {
+                break;
+            }
+        }
+        if (count($out) > 0) {
+            echo '      Fuse:'.implode(', ', $out).PHP_EOL;
+        }
+        
+        $c = $process->extract($search, $names);
+        $results = $c->toArray();
+        $out = [];
+        foreach ($results as $idx => $data) {
+            $out[] = $data[0].' ('.$data[1].'%)';
+            $scores[$data[0]] = (isset($scores[$data[0]]) ? $scores[$data[0]] : 0) + $data[1];
+            if ($idx >= $maxResults) {
+                break;
+            }
+        }
+        if (count($out) > 0) {
+            echo '      Fuzzy:'.implode(', ', $out).PHP_EOL;
+        }
+        
+        $levs = [];
+        foreach ($names as $name) {
+            $levs[$name] = levenshtein($search, $name);
+            //$scores[$name] = (isset($scores[$name]) ? $scores[$name] : 0) + (100 - ($levs[$name] * 15));
+        }
+        $levValues = array_values($levs);
+        $levValues = array_unique($levValues);
+        sort($levValues);
+        reset($levValues);
+        $found = 0;
+        $foundResults = 0;
+        $out = [];
+        foreach ($levValues as $levValue) {
+            $found++;
+            foreach ($levs as $name => $value) {
+                if ($value == $levValue) {
+                    $foundResults++;
+                    $out[] = $name.' ('.$value.')';
+                    $scores[$name] = (isset($scores[$name]) ? $scores[$name] : 0) + (100 - ($found * 10 + $value)); 
+                    if ($foundResults >= $maxResults) {
+                        break;
+                    }
+                }
+            }
+            if ($found >= 5 || $foundResults >= $maxResults) {
+                break;
+            }
+        }
+        echo '      Levenshtein:'.implode(', ', $out).PHP_EOL;
+        
+        /*
+        $scoreValues = array_values($scores);
+        $scoreValues = array_unique($scoreValues);
+        rsort($scoreValues);
+        reset($scoreValues);
+        $found = 0;
+        $foundResults = 0;
+        $out = [];
+        foreach ($scoreValues as $scoreValue) {
+            $found++;
+            foreach ($scores as $name => $value) {
+                if ($value == $scoreValue) {
+                    $foundResults++;
+                    $out[] = $name.' ('.$value.')';
+                    if ($foundResults >= $maxResults) {
+                        break;
+                    }
+                }
+            }
+            if ($found >= 5 || $foundResults >= $maxResults) {
+                break;
+            }
+        }
+        echo '      Overall:'.implode(', ', $out).PHP_EOL;
+        */
+    }
+}
+
+/*
+echo 'Found:'.PHP_EOL;
+foreach ($path2id as $path => $id) {
+    $platform = $platforms[$id];
+    $name = basename($path);
+    $developer = basename(dirname($path));
+    echo '    '.$developer.'/'.$name.'  =>  '.$platform['name'].PHP_EOL;
+}
+echo 'Missing:'.json_encode($missing, JSON_PRETTY_PRINT).PHP_EOL;
+*/
+echo 'Found '.count($path2id).' Missing '.count($missing).PHP_EOL;
