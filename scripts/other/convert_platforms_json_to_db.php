575b54ed36aa2400eccce7c96e195d382d94e338
Joe Huss <detain@interserver.net>
2019-07-31 01:02:09 -0400

    improvements to the importing with resetting data and handling fo auto increment ids

--- scripts/other/convert_platforms_json_to_db.php
+++ scripts/other/convert_platforms_json_to_db.php
@@ -2,6 +2,8 @@
 include '/storage/ConSolo/src/bootstrap.php';
 $platforms = json_decode(file_get_contents('/storage/ConSolo/json/platforms.json'), true);
 $db->query("delete from platforms");
+$db->query("truncate platform_matches");
+$db->query("alter table platforms auto_increment = 1;");
 foreach ($platforms as $platform => $data) {
 	$id = $db->insert('platforms')->cols(['name' => $platform])->query();
 	foreach ($data as $name => $types) {
@@ -9,6 +11,5 @@ foreach ($platforms as $platform => $data) {
 			$db->insert('platform_matches')->cols(['platform_id' => $id, 'name' => $name, 'type' => $type])->query();
 		}
 	}
-	echo 'Finished '.$platform.PHP_EOL;
 }
 
