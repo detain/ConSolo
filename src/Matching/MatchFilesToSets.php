73df9e8bd9b393477cc611d718a80d983e5b0351
Joe Huss <detain@interserver.net>
2019-07-31 10:26:09 -0400

    some improvements here and there

--- src/Matching/MatchFilesToSets.php
+++ src/Matching/MatchFilesToSets.php
@@ -14,34 +14,46 @@ use FuzzyWuzzy\Process;
 */
 global $db;
 
-$datFiles = $db->query("select id,type,name from dat_files order by name");
+$datFiles = $db->query("select id,type,name from dat_files where type != 'TOSEC-PIX' order by name");
+$outputGames = false;
+$outputSets = true;
 foreach ($datFiles as $datFileIdx => $datFileData) {
     $games = $db->query("select id,name from dat_games where file={$datFileData['id']}");
     $goodSet = true;
     $partialSet = false;
+    $goodCount = 0;
+    $badCount = 0;
     foreach ($games as $gameIdx => $gameData) {
         $roms = $db->query("select * from dat_roms where game={$gameData['id']}");
-        echo $datFileData['type'].'     '.$datFileData['name'].'    '.$gameData['name'];
+        if ($outputGames == true)
+            echo $datFileData['type'].'     '.$datFileData['name'].'    '.$gameData['name'];
         $good = true;
         $partial = false;
         foreach ($roms as $romIdx => $romData) {
             $files = $db->query("select path from files where size={$romData['size']} and md5='{$romData['md5']}'");
-            echo '  '.$romData['name'].' ';
+            if ($outputGames == true)
+                echo '  '.$romData['name'].' ';
             if (count($files) == 0) {
-                echo 'MISSING!';
+                if ($outputGames == true)
+                    echo 'MISSING!';
                 $good = false;
                 $goodSet = false;
+                $badCount++;
             } else {
+                $goodCount++;
                 $partialSet = true;
                 $partial = true;
                 $paths = [];
                 foreach ($files as $file) {
                     $paths[] = $file['path'];
                 }
-                echo ' FOUND! '.count($paths).' copies in "'.implode('", "', $paths).'"';
+                if ($outputGames == true)
+                    echo ' FOUND! '.count($paths).' copies in "'.implode('", "', $paths).'"';
             }
         }
-        echo '      Game '.($good == true ? 'GOOD' : ($partial == true ? 'PARTIAL' : 'BAD')).PHP_EOL;
+        if ($outputGames == true)
+            echo '      Game '.($good == true ? 'GOOD' : ($partial == true ? 'PARTIAL' : 'BAD')).PHP_EOL;
     }
-    echo $datFileData['type'].' '.$datFileData['name'].'    '.($goodSet == true ? 'GOOD' : ($partialSet == true ? 'PARTIAL' : 'BAD')).' SET'.PHP_EOL;
+    if ($outputSets == true)
+        echo $datFileData['type'].' '.$datFileData['name'].'    ['.$goodCount.'/'.($goodCount+$badCount).'] '.($goodSet == true ? 'GOOD' : ($partialSet == true ? 'PARTIAL' : 'BAD')).' SET'.PHP_EOL;
 }
