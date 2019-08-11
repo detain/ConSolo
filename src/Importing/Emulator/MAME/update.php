735e41a5d97b07741c881ca16bf62de6a8939f7b
Joe Huss <detain@interserver.net>
2019-08-10 23:40:03 -0400

    updates making it work better with reusing files

--- src/Importing/Emulator/MAME/update.php
+++ src/Importing/Emulator/MAME/update.php
@@ -52,8 +52,6 @@ function RunArray(&$data) {
     }
 }
 
-$tablePrefix = 'mame_';
-$tableSuffix = 's';
 $configKey = 'mame';
 $row = $db->query("select * from config where config.key='{$configKey}'");
 if (count($row) == 0) {
@@ -73,11 +71,17 @@ echo `wget -q https://github.com/mamedev/mame/releases/download/mame{$version}/m
 echo `rm -rf /tmp/update;`;
 echo `7z x -o/tmp/update mame.exe;`;
 unlink('mame.exe');
-echo 'Generating XML'.PHP_EOL;
-echo `cd /tmp/update; wine64 mame64.exe -listxml > xml.xml;`;
-echo 'Generating Software'.PHP_EOL;
-echo `cd /tmp/update; wine64 mame64.exe -listsoftware > software.xml;`;
 */
+if (!file_exists('/storage/data/xml/mame/xml-'.$version.'.xml')) {
+    echo 'Generating XML'.PHP_EOL;
+    echo `mame -listxml > /storage/data/xml/mame/xml-{$version}.xml;`;
+    //echo `cd /tmp/update; wine64 mame64.exe -listxml > /storage/data/xml/mame/xml-{$version}.xml;`;
+}
+if (!file_exists('/storage/data/xml/mame/software-'.$version.'.xml')) {
+    echo 'Generating Software'.PHP_EOL;
+    echo `mame -listsoftware > /storage/data/xml/mame/software-{$version}.xml;`;
+    //echo `cd /tmp/update; wine64 mame64.exe -listsoftware > /storage/data/xml/mame/software-{$version}.xml;`;
+}
 
 /*$txt = ['brothers', 'clones', 'crc', 'devices', 'full', 'media', 'roms', 'samples', 'slots', 'source'];
 foreach ($txt as $list) {
@@ -99,17 +103,20 @@ $xml = ['software', 'xml'];
 $removeXml = ['port','chip','display','sound','dipswitch','driver','feature','sample','device_ref','input','biosset','configuration','device','softwarelist','disk','slot','ramoption','adjuster'];
 foreach ($xml as $list) {
     echo "Getting {$list} List   ";
-    
-    $array = json_decode(file_get_contents('/storage/data/json/mame/'.$list.'.json'), TRUE);
-    /*
-    $xmlFile = '/tmp/update/'.$list.'.xml';
-    $string = file_get_contents($xmlFile);
-    echo "Parsing XML To Array   ";
-    $array = xml2array($string, 1, 'attribute');
-    unset($string);
-    echo "Simplifying Array   ";
-    RunArray($array);
-    */
+    if (!file_exists('/storage/data/json/mame/'.$list.'-'.$version.'.json')) {    
+        $xmlFile = '/storage/data/xml/mame/'.$list.'-'.$version.'.xml';
+        $string = file_get_contents($xmlFile);
+        echo "Parsing XML To Array   ";
+        $array = xml2array($string, 1, 'attribute');
+        unset($string);
+        echo "Simplifying Array   ";
+        RunArray($array);
+        echo "Writing to JSON {$list}.json   ";
+        file_put_contents('/storage/data/json/mame/'.$list.'-'.$version.'.json', json_encode($array, JSON_PRETTY_PRINT));
+    } else {
+        $array = json_decode(file_get_contents('/storage/data/json/mame/'.$list.'-'.$version.'.json'), TRUE);
+    }
+    echo 'Parsing Into DB ';
     if ($list == 'software') {
         $games = [];
         foreach ($array['softwarelists']['softwarelist'] as $idx => $software) {
@@ -176,9 +183,6 @@ foreach ($xml as $list) {
             }
         }
     }
-    //echo "Writing to JSON {$list}.json   ";
-    //file_put_contents($list.'.json', json_encode($array, JSON_PRETTY_PRINT));
     echo "done\n";
-    @unlink($xmlFile);
 }
 echo `rm -rf /tmp/update;`;
