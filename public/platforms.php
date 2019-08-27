1f45b28cb1ffbd549f6b05f9b37fff70da8485ed
Joe Huss <detain@interserver.net>
2019-08-26 22:51:39 -0400

    twig updates

--- public/platforms.php
+++ public/platforms.php
@@ -5,6 +5,10 @@ require_once __DIR__.'/../src/bootstrap.php';
 * @var \Workerman\MySQL\Connection
 */
 global $db;
+/**
+* @var \Twig\Environment
+*/
+global $twig;
 $results = $db->query("select * from config");
 $sources = [];
 $newSource = [
@@ -54,84 +58,16 @@ $sources['nointro']['roms'] = ($db->column("SELECT count(*) FROM dat_files left 
 $sources['tosec']['roms'] = ($db->column("SELECT count(*) FROM dat_files left join dat_games on dat_files.id=file left join dat_roms on dat_games.id=game where type='TOSEC' and dat_games.id is not null and dat_roms.id is not null"))[0];
 $sources['toseciso']['roms'] = ($db->column("SELECT count(*) FROM dat_files left join dat_games on dat_files.id=file left join dat_roms on dat_games.id=game where type='TOSEC-ISO' and dat_games.id is not null and dat_roms.id is not null"))[0];
 $sources['redump']['roms'] = ($db->column("SELECT count(*) FROM dat_files left join dat_games on dat_files.id=file left join dat_roms on dat_games.id=game where type='Redump' and dat_games.id is not null and dat_roms.id is not null"))[0];
+$sources['oldcomputers'] = $newSource;
+$sources['oldcomputers']['name'] = 'Old-Computers';
+$sources['oldcomputers']['platforms'] = ($db->column("SELECT count(*) FROM oldcomputers_platforms"))[0];
+$sources['oldcomputers']['emulators'] = ($db->column("SELECT count(*) FROM oldcomputers_emulators"))[0];
+ 
 //echo '<pre style="text-align: left;">';print_r($versions);echo '</pre>';exit;  
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta http-equiv="Lang" content="en">
-    <meta name="author" content="Joe Huss">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <title>ConSolo</title>
-    <link rel="stylesheet" type="text/css" href="my.css">
-    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
-    <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
-    <link href="https://nostalgic-css.github.io/NES.css/style.css" rel="stylesheet" />
-    <link href="https://unpkg.com/dialog-polyfill@latest/dist/dialog-polyfill.css" rel="stylesheet" />
-</head>
-<body>
-    <div style="height: 50px;">
-        <i style="float: left; margin-left: 10px;" class="nes-logo"></i>
-        <h1 style="float: left; margin-left: 10px;">ConSolo</h1>
-        <i style="float: left; margin-left: 10px;" class="snes-logo"></i>
-    </div>
-    <br>
-    <p>
-        <a class="nes-btn is-success" href="index.html">About</a>
-        <a class="nes-btn is-warning" href="status.php">Status</a>
-        <a class="nes-btn is-error" href="match.php">Matches</a>
-        <a class="nes-btn is-primary" href="emulators.php">Emulators</a>
-        <a class="nes-btn is-disabled" href="platforms.php">Platforms</a>
-        <a class="nes-btn is-warning" href="games.php">Games</a>
-        <a class="nes-btn is-primary" href="roms.php">ROMs</a>
-        <a class="nes-btn is-error" href="https://nostalgic-css.github.io/NES.css/" target="_blank">Theme</a>
-    </p>
-    <br>    
-    <section class="showcase">
-        <section class="nes-container with-title">
-            <h3 class="title">Status</h3>
-            <h3>Data Sources</h3>
-            <div class="nes-table-responsive">
-                <table class="nes-table is-bordered is-centered">
-                    <thead>
-                        <tr>
-                            <th>Source</th>
-                            <th>Version</th>
-                            <th>Platforms</th>
-                            <th>Emulators</th>
-                            <th>Games</th>
-                            <th>ROMs</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-<?php
-foreach ($sources as $key => $data) {
-    echo '
-                        <tr>
-                            <td>'.$data['name'].'</td>
-                            <td>'.$data['version'].'</td>
-                            <td>'.$data['platforms'].'</td>
-                            <td>'.$data['emulators'].'</td>
-                            <td>'.$data['games'].'</td>
-                            <td>'.$data['roms'].'</td>
-                        </tr>';
-}                        
-?>                    
-                    </tbody>
-                </table>
-            </div>            
-        </section>
-    </section>
-    <script src="https://unpkg.com/dialog-polyfill@latest/dist/dialog-polyfill.js"></script>
-    <script>
-        var dialogs = document.querySelectorAll('dialog');
-        for (var dialog in dialogs) {
-            if (typeof dialogs[dialog] == "object") {
-                dialogPolyfill.registerDialog(dialogs[dialog]);
-            }
-        }
-    </script>  
-</body>
-</html>
+
+echo $twig->render('status.twig', array(
+    'sources' => $sources,
+//    'client_id' => $_GET['client_id'],
+//    'response_type' => $_GET['response_type'],
+    'queryString' => $_SERVER['QUERY_STRING']
+));
