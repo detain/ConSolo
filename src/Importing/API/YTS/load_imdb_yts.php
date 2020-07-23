c71a090e186a25a417242032b21c57f49ae4a963
Joe Huss <detain@interserver.net>
2020-07-23 04:52:43 -0500

    updated the yts importer

--- src/Importing/API/YTS/load_imdb_yts.php
+++ src/Importing/API/YTS/load_imdb_yts.php
@@ -1,37 +1,43 @@
 <?php
-require_once __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__.'/inc.php';
+require_once __DIR__.'/../../../bootstrap.php';
 $imdbFields = ['alsoknow','cast','colors','comment','composer','country','crazy_credits','director','episodes','genre','genres','get_episode_details','goofs','imdbsite','is_serial','keywords','languages','locations','main_url','movietype','mpaa','mpaa_reason','orig_title','photo_localurl','plot','plotoutline','producer','quotes','rating','runtime','seasons','sound','soundtrack','tagline','taglines','title','trailers','trivia','votes','writing','year'];
-$files = loadJson('yts');
-$imdb = loadJson('imdb');
+/**
+* @var \Workerman\MySQL\Connection
+*/
+global $db;
+global $config;
+$imdbIds = [];
+$result = $db->query("select imdb_code from yts");
+foreach ($result as $data) {
+	$imdbIds[] = $data['imdb_code'];
+}
+$existingIds = [];
+$result = $db->query("select id from imdb where id in (select imdb_code from yts)");
+foreach ($result as $data) {
+	$existingIds[] = $data['id'];
+}
 $updates = 0;
-$saveEverySec = 240;
-$startTime = time();
-foreach ($files as $ytsId => $file) {
-	echo '# '.$file['title'].' ('.$file['year'].')'.PHP_EOL;
-	if (isset($file['imdb_code'])) {
-		$imdbCode = preg_replace('/^tt/','', $file['imdb_code']);
-		if (array_key_exists($imdbCode, $imdb)) {
-			echo '    Already Matched, Skipping!'.PHP_EOL;
-		} else {
-			$title = new \Imdb\Title($imdbCode);
-			$imdb[$imdbCode] = [];
-			foreach ($imdbFields as $field) {
-				if (method_exists($title, $field)) {
-					$imdb[$imdbCode][$field] = $title->$field();
-				} else {
-					$imdb[$imdbCode][$field] = $title->$field;
-				}
-			}            
-			$updates++;                   
+foreach ($imdbIds as $imdbId) {
+	echo '# '.$imdbId.PHP_EOL;
+	if (!in_array($imdbId, $existingIds)) {
+		$imdbCode = preg_replace('/^tt/','', $imdbId);
+		$title = new \Imdb\Title($imdbCode);
+		$imdb = [];
+		foreach ($imdbFields as $field) {
+			if (method_exists($title, $field)) {
+				$imdb[$field] = $title->$field();
+			} else {
+				$imdb[$field] = $title->$field;
+			}
 		}
-	}
-	$now = time();
-	if ($now - $startTime >= $saveEverySec) {
-		putJson('imdb', $imdb);
-		echo 'Wrote JSON with '.$updates.' updates'.PHP_EOL;		
-		$startTime = $now;
+		$db->insert('imdb')
+			->cols([
+				'id' => $imdbId, 
+				'doc' => json_encode($imdb)
+			])
+			->lowPriority($config['db_low_priority'])
+			->query();            
+		$updates++;                   
 	}
 }
-putJson('imdb', $imdb);
 echo 'Loaded '.$updates.' Movies!'.PHP_EOL;
