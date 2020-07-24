324bffb035a591c60a4cb50a100688c4f9f75459
Joe Huss <detain@interserver.net>
2020-07-24 00:26:19 -0500

    updates fixing up the list and list js interfaces and updating hte one vue pages ajax urls

--- public/list.php
+++ public/list.php
@@ -1,16 +1,11 @@
 <?php
-require_once __DIR__.'/../../vendor/autoload.php';
-$movies =  json_decode(file_get_contents('data/imdb.json'), true);
+require_once __DIR__.'/../src/bootstrap.php';
+
+/**
+* @var \Workerman\MySQL\Connection
+*/
+global $db;
 $max = 100;
-$count = 0;
-$return = [];
-foreach ($movies as $movieName => $movieData) {
-	$count++;
-	if ($count >= $max)
-		break;
-	$movieData['id'] = $movieName;
-	$return[] = $movieData;
-}
-file_put_contents('data/imdb_small.json', json_encode($return));
+$response = $db->query("select * from imdb limit {$max}");
 header('Content-type: application/json; charset=UTF-8');
-echo json_encode($return);
+echo json_encode($response);
--- public/list_js.php
+++ public/list_js.php
@@ -1,14 +1,11 @@
 <?php
-require_once __DIR__.'/../../vendor/autoload.php';
-$movies =  json_decode(file_get_contents('data/imdb_small.json'), true);
-$max = 1000;
-$count = 0;
-$return = [];
-foreach ($movies as $movieName => $movieData) {
-	$count++;
-	if ($count >= $max)
-		break;
-	$return[] = $movieData;
-}
+require_once __DIR__.'/../src/bootstrap.php';
+
+/**
+* @var \Workerman\MySQL\Connection
+*/
+global $db;
+$max = 100;
+$response = $db->query("select * from imdb limit {$max}");
 header('Content-type: application/javascript; charset=UTF-8');
-echo 'var result = '.json_encode($return).';'.PHP_EOL;
+echo 'var result = '.json_encode($response).';'.PHP_EOL;
