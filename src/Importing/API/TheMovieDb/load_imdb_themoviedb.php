d03cf4aa0fcde44e2ab3212d5f39644e518f8b95
Joe Huss <detain@interserver.net>
2020-08-11 23:09:35 -0500

    updates

--- src/Importing/API/TheMovieDb/load_imdb_themoviedb.php
+++ src/Importing/API/TheMovieDb/load_imdb_themoviedb.php
@@ -64,13 +64,13 @@ $imdbIdsNew = array_slice($imdbIds, $start, $partSize);
 $imdbIds = $imdbIdsNew;
 unset($imdbIdsNew);
 $total = count($imdbIds);
-echo 'Divided them into a section of '.$total.' ids'.PHP_EOL;
+echo '['.$part.'/'.$divide.'] '.(isset($ip) ? 'IP '.$ip.' ' : '').'Divided them into a section of '.$total.' ids'.PHP_EOL;
 foreach ($imdbIds as $imdbId) {
 	if (trim($imdbId) == '')
 		continue;
 	if (file_exists(__DIR__.'/stop'))
 		break;
-	echo (isset($ip) ? 'IP '.$ip.' ' : '').'# '.$imdbId.' '.$updates.'/'.$total.PHP_EOL;
+	echo '['.$part.'/'.$divide.'] '.(isset($ip) ? 'IP '.$ip.' ' : '').'# '.$imdbId.' '.$updates.'/'.$total.PHP_EOL;
 	//if (!in_array($imdbId, $existingIds)) {
 		$imdbCode = preg_replace('/^tt/','', $imdbId);
 		$title = new \Imdb\Title($imdbCode, $imdbConfig);
@@ -80,7 +80,7 @@ foreach ($imdbIds as $imdbId) {
 				try {
 					$imdb[$field] = $title->$field();
 				} catch (\Imdb\Exception\Http $e) {
-					echo (isset($ip) ? 'IP '.$ip.' ' : '')."exception error ".$e->getMessage().PHP_EOL;
+					echo '['.$part.'/'.$divide.'] '.(isset($ip) ? 'IP '.$ip.' ' : '')."exception error ".$e->getMessage().PHP_EOL;
 					if (isset($imdb[$field])) {
 						unset($imdb[$field]);
 					}
@@ -99,8 +99,8 @@ foreach ($imdbIds as $imdbId) {
 				->query();            
 			$updates++;                   
 		} catch  (\PDOException $E) {
-			echo (isset($ip) ? 'IP '.$ip.' ' : '')."Exception error ".$e->getMessage().PHP_EOL;
+			echo '['.$part.'/'.$divide.'] '.(isset($ip) ? 'IP '.$ip.' ' : '')."Exception error ".$e->getMessage().PHP_EOL;
 		}
 	//}
 }
-echo 'Loaded '.$updates.' Movies!'.PHP_EOL;
+echo '['.$part.'/'.$divide.'] '.(isset($ip) ? 'IP '.$ip.' ' : '').'Loaded '.$updates.' Movies!'.PHP_EOL;
