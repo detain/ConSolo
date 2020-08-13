ff126f8ff2fc82c3cb61a87e02cf846e5e9bd278
Joe Huss <detain@interserver.net>
2020-08-13 01:22:15 -0500

    torrent update

--- src/Importing/Torrent/read_torrent.php
+++ src/Importing/Torrent/read_torrent.php
@@ -1,14 +1,16 @@
 <?php
 require_once 'Torrent.php';
 
-$torrents = [];
-$count = 0;
-foreach (glob('t/*') as $file) {
-	$count++;
-	$id = basename($file);
+//$torrents = [];
+//$count = 0;
+//foreach (glob('t/*') as $file) {
+//	$count++;
+//	$id = basename($file);
+$file = 'C6C1417AD4C60B6532ECA4ED1377C51C78E4B90B';
 	$torrent = new Torrent($file);
-	$torrents[$id] = $torrent->content();
-	echo $count.PHP_EOL;
-}
-file_put_contents('torrentfiles.json', json_encode($torrents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+print_r($torrent->content());
+//	$torrents[$id] = $torrent->content();
+//	echo $count.PHP_EOL;
+//}
+//file_put_contents('torrentfiles.json', json_encode($torrents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
