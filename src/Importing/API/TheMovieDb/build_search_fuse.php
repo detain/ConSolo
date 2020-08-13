225d93fe7afdfc46c03794405d52bdfb16d793e1
Joe Huss <detain@interserver.net>
2020-08-13 02:50:47 -0500

    updates to fuse data generation

--- src/Importing/API/TheMovieDb/build_search_fuse.php
+++ src/Importing/API/TheMovieDb/build_search_fuse.php
@@ -5,45 +5,75 @@
 
 require_once __DIR__.'/../../../bootstrap.php';
 
-$limit = 1000;
+$fuseWeights = [
+	"keys" => [
+		["name" => "year", "weight" => 0.3],
+		["name" => "title", "weight" => 0.7]
+]];
+$limit = 25000;
 $offset = 0;
 $end = false;
 $fuseData = [];
+echo "Loading IMDB starting at:";
 while (!$end) {
-	//echo "Loading IMDB starting at $offset";
+	echo " $offset";
 	$tempFiles = $db->query("select * from imdb limit $offset, $limit");
 	foreach ($tempFiles as $idx => $data) {
 		$doc = json_decode($data['doc'], true);
-		$fuse = [
-			'title' => $data['title'],
-			'year' => $data['year']
-		];
-		$fuseData[] = $fuse;
+		$titles = [];
+		$titles[] = $data['title'];
+		if (isset($doc['alsoknow']))
+			foreach ($doc['alsoknow'] as $alsoIdx => $alsoKnow)
+				if (!in_array($alsoKnow['title'], $titles))
+					$titles[] = $alsoKnow['title'];
+		if (isset($doc['orig_title']) && !in_array($doc['orig_title'], $titles))
+			$titles[] = $doc['orig_title'];
+		foreach ($titles as $title)
+			$fuseData[] = [
+				'id' => $data['id'],
+				'title' => $data['title'],
+				'year' => $data['year']
+			];
 	}
 	$offset += $limit;
 	$end = count($tempFiles) < $limit;
 }
-$fuse = new \Fuse\Fuse($fuseData);
+echo '  done'.PHP_EOL;
+echo 'caching data';
+file_put_contents(__DIR__.'/../../../../data/json/imdb_fuse.json', json_encode($fuseData, JSON_PRETTY_PRINT));
+echo '  done'.PHP_EOL;
+$fuse = new \Fuse\Fuse($fuseData, $fuseWeights);
 $results = $fuse->search('hamil');
 print_r($results);
 
 $offset = 0;
 $end = false;
 $fuseData = [];
+echo "Loading TMDB starting at:";
 while (!$end) {
-	//echo "Loading TMDB starting at $offset";
+	echo " $offset";
 	$tempFiles = $db->query("select * from tmdb limit $offset, $limit");
 	foreach ($tempFiles as $idx => $data) {
 		$doc = json_decode($data['doc'], true);
-		$fuse = [
-			'title' => $data['title'],
-			'year' => substr($data['release_date'], 0, 4)
-		];
-		$fuseData[] = $fuse;
+		$titles = [];
+		$titles[] = $data['title'];
+		if (isset($doc['original_title']) && !in_array($doc['original_title'], $titles))
+			$titles[] = $doc['original_title'];
+		$year = substr($data['release_date'], 0, 4);
+		foreach ($titles as $title)
+			$fuseData[] = [
+				'id' => $data['id'],
+				'title' => $data['title'],
+				'year' => $year
+			];
 	}
 	$offset += $limit;
 	$end = count($tempFiles) < $limit;
 }
-$fuse = new \Fuse\Fuse($fuseData);
+echo '  done'.PHP_EOL;
+echo 'caching data';
+file_put_contents(__DIR__.'/../../../../data/json/tmdb_fuse.json', json_encode($fuseData, JSON_PRETTY_PRINT));
+echo '  done'.PHP_EOL;
+$fuse = new \Fuse\Fuse($fuseData, $fuseWeights);
 $results = $fuse->search('hamil');
 print_r($results);
