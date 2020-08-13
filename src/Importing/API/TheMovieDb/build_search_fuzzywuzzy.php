c9b92ca6e2de0c752c38da94a2476c703b9df418
Joe Huss <detain@interserver.net>
2020-08-13 02:17:59 -0500

    updates adding implementations of fuzzy search libraries for imdb and themoviedb matching

---
+++ src/Importing/API/TheMovieDb/build_search_fuzzywuzzy.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* @link https://github.com/loilo/Fuse 
+*/
+
+require_once __DIR__.'/../../../bootstrap.php';
+
+$limit = 1000;
+$offset = 0;
+$end = false;
+$fuseData = [];
+while (!$end) {
+	//echo "Loading IMDB starting at $offset";
+	$tempFiles = $db->query("select * from imdb limit $offset, $limit");
+	foreach ($tempFiles as $idx => $data) {
+		$doc = json_decode($data['doc'], true);
+		$fuse = [
+			'title' => $data['title'],
+			'year' => $data['year']
+		];
+		$fuseData[] = $fuse;
+	}
+	$offset += $limit;
+	$end = count($tempFiles) < $limit;
+}
+$fuse = new \Fuse\Fuse($fuseData);
+$results = $fuse->search('hamil');
+print_r($results);
+
+$offset = 0;
+$end = false;
+$fuseData = [];
+while (!$end) {
+	//echo "Loading TMDB starting at $offset";
+	$tempFiles = $db->query("select * from tmdb limit $offset, $limit");
+	foreach ($tempFiles as $idx => $data) {
+		$doc = json_decode($data['doc'], true);
+		$fuse = [
+			'title' => $data['title'],
+			'year' => substr($data['release_date'], 0, 4)
+		];
+		$fuseData[] = $fuse;
+	}
+	$offset += $limit;
+	$end = count($tempFiles) < $limit;
+}
+$fuse = new \Fuse\Fuse($fuseData);
+$results = $fuse->search('hamil');
+print_r($results);
--- src/Importing/Torrent/read_torrent.php
+++ src/Importing/Torrent/read_torrent.php
@@ -1,16 +1,20 @@
 <?php
 require_once 'Torrent.php';
 
-//$torrents = [];
-//$count = 0;
-//foreach (glob('t/*') as $file) {
-//	$count++;
-//	$id = basename($file);
 $file = 'C6C1417AD4C60B6532ECA4ED1377C51C78E4B90B';
-	$torrent = new Torrent($file);
-print_r($torrent->content());
+$torrent = new Torrent('/storage/local/ConSolo/data/torrent/'.$file);
+//echo 'private: ', $torrent->is_private() ? 'yes' : 'no'.PHP_EOL;
+//echo 'annonce: '.print_r($torrent->announce(),true).PHP_EOL;
+echo 'name: ', $torrent->name().PHP_EOL; 
+//echo 'comment: ', $torrent->comment().PHP_EOL; 
+//echo 'piece_length: ', $torrent->piece_length().PHP_EOL; 
+//echo 'size: ', $torrent->size( 2 ).PHP_EOL;
+//echo 'hash info: ', $torrent->hash_info().PHP_EOL;
+echo 'content: ';
+var_dump( $torrent->content() );
+//echo 'source: '.$torrent.PHP_EOL;
+//echo 'stats: ';
+//var_dump( $torrent->scrape() );
+//echo PHP_EOL;
 //	$torrents[$id] = $torrent->content();
 //	echo $count.PHP_EOL;
-//}
-//file_put_contents('torrentfiles.json', json_encode($torrents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-
