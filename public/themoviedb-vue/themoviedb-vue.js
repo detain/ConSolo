d3ad7468c11898a22782b2ab161e32479eae65b1
Joe Huss <detain@interserver.net>
2020-08-18 13:09:28 -0500

    updates to the backend code for some of the vue scipts

--- public/themoviedb-vue/themoviedb-vue.js
+++ public/themoviedb-vue/themoviedb-vue.js
@@ -1,17 +1,17 @@
 new Vue({
   el: "#app",
   data: {
-    items: [],
-    image: 'https://image.tmdb.org/t/p/w342',
-    loaded: true },
+	items: [],
+	image: 'https://image.tmdb.org/t/p/w342',
+	loaded: true },
 
   created: function () {
-    this.fetchData();
+	this.fetchData();
   },
   methods: {
-    fetchData: function () {
-      this.$http.get('https://api.themoviedb.org/3' + '/discover/tv?api_key=' + 'bc7aff7c3e4cd31438852dcac726059e' + '&sort_by=popularity.desc').then(response => {
-        this.items = response.body;
-        this.loaded = false;
-      });
-    } } });
+	fetchData: function () {
+	  this.$http.get('https://vault3.is.cc/consolo/public/list.php?api_key=' + 'bc7aff7c3e4cd31438852dcac726059e' + '&sort_by=popularity.desc').then(response => {
+		this.items = response.body;
+		this.loaded = false;
+	  });
+	} } });
