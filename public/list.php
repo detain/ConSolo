89f32ebdfc2b3e795132f0938b347bc582311da8
Joe Huss <detain@interserver.net>
2020-08-19 04:03:47 -0500

    updates to tmdb_movie

--- public/list.php
+++ public/list.php
@@ -10,7 +10,7 @@ $response = [
 	'page' => 1,
 	'total_results' => 1,
 	'total_pages' => 1,
-	'results' => $db->query("select title, poster_path, vote_average, overview from movies left join tmdb on tmdb_id=tmdb.id order by title limit {$max}")
+	'results' => $db->query("select title, poster_path, vote_average, overview from movies left join tmdb_movie on tmdb_id=tmdb_movie.id order by title limit {$max}")
 ];
 $result = $db->query("select count(*) as count from movies");
 $response['total_results'] = $result[0]['count']; 
