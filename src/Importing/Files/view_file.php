9899b575d223cd1db32c387ff5ac24484d26f537
Joe Huss <detain@interserver.net>
2020-08-24 15:43:51 -0500

    update adding view_file script

---
+++ src/Importing/Files/view_file.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Scans through an array of path globs finding all files and scanning them performing
+* checksumming, and storing of basic information.  This process can take a long time on
+* a larger set of files.  It also scans inside compressed files. 
+*/
+
+require_once __DIR__.'/../../bootstrap.php';
+
+
+
+/**
+* @var \Workerman\MySQL\Connection
+*/
+global $db;
+global $files, $db, $paths, $skipGlobs, $compressionTypes, $tmpDir, $scanCompressed, $hashAlgos, $compressedHashAlgos, $maxSize, $useMaxSize, $useMagic;
+
+$result = $db->row("select * from files where id={$_SERVER['argv'][1]}");
+$result['extra'] = json_decode($result['extra'], true);
+print_r($result);
