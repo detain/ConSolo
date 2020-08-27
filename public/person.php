16083570316441c18e787a7c60155ce3034ee0f5
Joe Huss <detain@interserver.net>
2020-08-26 22:32:06 -0500

    added person list

---
+++ public/person.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__.'/../src/bootstrap.php';
+$imdbFields = ['alsoknow','cast','colors','comment','composer','country','crazy_credits','director','episodes','genre','genres','get_episode_details','goofs','imdbsite','is_serial','keywords','languages','locations','main_url','movietype','mpaa','mpaa_reason','orig_title','photo_localurl','plot','plotoutline','producer','quotes','rating','runtime','seasons','sound','soundtrack','tagline','taglines','title','trailers','trivia','votes','writing','year'];
+/**
+* @var \Workerman\MySQL\Connection
+*/
+global $db;
+global $config, $curl_config;
+$response = [];
+if (isset($_REQUEST['id'])) {
+	$id = (int)$_REQUEST['id'];
+	$json = json_decode($db->single("select doc from tmdb_person where id={$id} limit 1"), true);
+	$response['status'] = 'ok';
+	$response['movie'] = $json;
+} else {
+	$response['status'] = 'error';
+}
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode($response);
