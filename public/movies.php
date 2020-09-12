7c3c92284547a6167db5fb9ceb8692c9adf84492
Joe Huss <detain@interserver.net>
2020-09-12 10:45:26 -0500

    updates to the movies and movie pages adding some more well defined url paths and endpoints that  will match the intial setup

--- public/movies.php
+++ public/movies.php
@@ -9,8 +9,8 @@ global $db;
 */
 global $twig;
 global $config, $curl_config, $hostId;
-$response = [];
-$rows = $db->query("select title, poster_path, vote_average, overview, release_date from movies left join tmdb_movie on tmdb_movie.id=movies.tmdb_id left join files on file_id=files.id where host={$hostId} and title is not null order by title");
+$limit = 100;
+$rows = $db->query("select tmdb_movie.id, title, poster_path, vote_average, overview, release_date from movies left join tmdb_movie on tmdb_movie.id=movies.tmdb_id left join files on file_id=files.id where host={$hostId} and title is not null order by title limit {$limit}");
 echo $twig->render('movies.twig', array(
 	'results' => $rows,
 //    'client_id' => $_GET['client_id'],
