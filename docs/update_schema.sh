6967e070aaab968c2fc9c59c62caa5f0d28d63dc
Joe Huss <detain@interserver.net>
2020-09-12 07:49:09 -0500

    updated the update schema script to hide the timestamp of the dump for better handling

--- docs/update_schema.sh
+++ docs/update_schema.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysqldump --set-gtid-purged=off --disable-keys -d consolo |sed s#" AUTO_INCREMENT=[0-9]*"#""#g > schema.sql
+mysqldump --set-gtid-purged=off --disable-keys -d consolo |sed s#" AUTO_INCREMENT=[0-9]*"#""#g |grep -v "^-- Dump completed on" > schema.sql
