372530dd1ed43d379e8f6a6985edc0da5a108a22
Joe Huss <detain@interserver.net>
2020-09-12 04:56:35 -0500

    work on the movie details page adding dissplaying a video player with link to the file if there i s a file match

--- public/movie.php
+++ public/movie.php
@@ -8,14 +8,16 @@ global $db;
 * @var \Twig\Environment
 */
 global $twig;
-global $config, $curl_config;
+global $config, $curl_config, $hostId;
 $response = [];
 if (isset($_REQUEST['id'])) {
 	$id = (int)$_REQUEST['id'];
 	$json = json_decode($db->single("select doc from tmdb_movie where id={$id} limit 1"), true);
 }
+$fileId = $db->single("select id from files where tmdb_id={$json['id']} and host={$hostId}");
 echo $twig->render('movie.twig', array(
 	'movie' => $json,
+	'fileId' => $fileId,
 //    'client_id' => $_GET['client_id'],
 //    'response_type' => $_GET['response_type'],
 	'queryString' => $_SERVER['QUERY_STRING']
