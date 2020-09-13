184db5e1143efd5575ba46a8ab13f9c1a1aba34e
Joe Huss <detain@interserver.net>
2020-09-13 00:56:43 -0500

    updates adding nikic/php-parser

--- public/movie.php
+++ public/movie.php
@@ -15,10 +15,8 @@ if (isset($_REQUEST['id'])) {
 	$json = json_decode($db->single("select doc from tmdb_movie where id={$id} limit 1"), true);
 }
 $fileId = $db->single("select id from files where tmdb_id={$json['id']} and host={$hostId}");
-echo $twig->render('movie.twig', array(
+echo $twig->render('movie.twig', [
 	'movie' => $json,
 	'fileId' => $fileId,
-//    'client_id' => $_GET['client_id'],
-//    'response_type' => $_GET['response_type'],
 	'queryString' => $_SERVER['QUERY_STRING']
-));
+]);
--- public/movies.php
+++ public/movies.php
@@ -11,9 +11,7 @@ global $twig;
 global $config, $curl_config, $hostId;
 $limit = 100;
 $rows = $db->query("select tmdb_movie.id, title, poster_path, vote_average, overview, release_date from movies left join tmdb_movie on tmdb_movie.id=movies.tmdb_id left join files on file_id=files.id where host={$hostId} and title is not null order by title limit {$limit}");
-echo $twig->render('movies.twig', array(
+echo $twig->render('movies.twig', [
 	'results' => $rows,
-//    'client_id' => $_GET['client_id'],
-//    'response_type' => $_GET['response_type'],
 	'queryString' => $_SERVER['QUERY_STRING']
-));
+]);
