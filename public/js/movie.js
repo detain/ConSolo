628859558202c3391ba962ee0f13695a295e4133
Joe Huss <detain@interserver.net>
2020-09-24 01:29:33 -0500

    updates to movie page fixing json data / improving the loading

--- public/js/movie.js
+++ public/js/movie.js
@@ -1,11 +1,10 @@
-const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get('id');
+//const urlParams = new URLSearchParams(window.location.search);
+//const movieId = urlParams.get('id');
 $(document).ready(function () {
-
-	$.getJSON("https://consolo.is.cc/item.php?type=movie&id="+movieId, function (data) {
-		console.log(data);
-		$("#json").JSONView(data, { collapsed: true });
-	});
+	//$.getJSON("https://consolo.is.cc/item.php?type=movie&id="+movieId, function (data) {
+		//$("#json").JSONView(data, { collapsed: true });
+	//});
+	$("#json").JSONView(movie, { collapsed: true });
 	$('.nav-sidebar li a[data-toggle="tab"]').click(function (e) {
 		e.preventDefault();
 		$(".nav-sidebar li.active").removeClass("active");
