6cbf62668064ef18f96bb9bc40a28cfb7e248e62
Joe Huss <detain@interserver.net>
2020-09-26 04:10:18 -0500

    update

--- src/Importing/Web/emucr.com/update_emucr_emulators.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IFS="
-"
-# grabs all the story archive dates and loads the archive page for each grabbing all the artile titles/urls
-for i in $(curl -s https://www.emucr.com/|grep "<a href.*archive.htm"|cut -d\' -f2); do
-	echo -e "\n\n--- $i ---\n";
-	curl -s "$i"|grep "<a href.*title=" ;
-done > emucr.html
-
