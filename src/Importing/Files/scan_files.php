eea9a26f9f0ca00bf2ce88e4540d259ed4fde955
Joe Huss <detain@interserver.net>
2020-10-03 13:05:14 -0500

    minor fixs

--- src/Importing/Files/scan_files.php
+++ src/Importing/Files/scan_files.php
@@ -350,8 +350,6 @@ function updateFile($path)  {
 			foreach ($fields as $idx => $row) {
 				$field = $row['desc']['name'];
 				$field = str_replace(['/',' ', '.', '#'], ['', '_', '', 'num'], strtolower($field));
-				if (in_array($field, ['dt_flags', 'dat_flags_1']))
-					continue;
 				if ($row['type'] == 'STRING') {
 					$data[$field] = $row['data']; 
 				} elseif ($row['type'] == 'DATETIME') {
