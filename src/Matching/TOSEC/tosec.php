d146e0dfc9f8607a58788ab07e13171e316519a6
Joe Huss <detain@interserver.net>
2020-10-14 00:39:25 -0500

    updates adding tosec-pix

--- src/Matching/TOSEC/tosec.php
+++ src/Matching/TOSEC/tosec.php
@@ -1,14 +1,17 @@
 <?php
-`wget -q "https://www.tosecdev.org/downloads/category/50-2020-07-29?download=99:tosec-dat-pack-complete-3036-tosec-v2020-07-29" -O tosec.zip;unzip -qq -o -d tosec tosec.zip;rm -f tosec.zip;`;
+$invalid = ['Magazines', 'Newsletters', 'Manuals', 'Books', 'Commercials', 'Comics', 'Video', 'Catalogs', 'Artbooks', 'Game Guides', 'TV Series'];
+//`wget -q "https://www.tosecdev.org/downloads/category/50-2020-07-29?download=99:tosec-dat-pack-complete-3036-tosec-v2020-07-29" -O tosec.zip;unzip -qq -o -d tosec tosec.zip;rm -f tosec.zip;`;
 $platforms = [];
-foreach (['TOSEC', 'TOSEC-ISO'] as $type) {
+foreach (['TOSEC', 'TOSEC-ISO', 'TOSEC-PIX'] as $type) {
 	preg_match_all('/mkdir "(?P<manufacturer>[^\\\\"]+)\\\\(?P<name>[^\\\\"]+)[\\\\"].*$/muU', file_get_contents('tosec/Scripts/create folders/'.$type.'_folders.bat'), $matches);
 	foreach ($matches['manufacturer'] as $idx => $manufacturer) {
+		if (in_array($matches['name'][$idx], $invalid))
+			continue;
 		if (!array_key_exists($manufacturer, $platforms))
 			$platforms[$manufacturer] = [];
 		if (!in_array($matches['name'][$idx], $platforms[$manufacturer]))
 			$platforms[$manufacturer][] = $matches['name'][$idx];
 	}
 }
-`rm -rf tosec;`;
+//`rm -rf tosec;`;
 echo json_encode($platforms, JSON_PRETTY_PRINT);
