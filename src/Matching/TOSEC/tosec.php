e0f47a88132c0e37784eb6d5342085a1d7420cca
Joe Huss <detain@interserver.net>
2020-10-13 22:29:54 -0500

    updates to tosec platform parser switching it to using preg_match_all and file_get_contents insatead of shell code and fixing up the duplicates

--- src/Matching/TOSEC/tosec.php
+++ src/Matching/TOSEC/tosec.php
@@ -1,12 +1,14 @@
 <?php
-`wget "https://www.tosecdev.org/downloads/category/50-2020-07-29?download=99:tosec-dat-pack-complete-3036-tosec-v2020-07-29" -O tosec.zip;unzip -o -d tosec tosec.zip;rm -f tosec.zip;`;
-$cmd='grep mkdir "tosec/Scripts/create folders/TOSEC_folders.bat" "tosec/Scripts/create folders/TOSEC-ISO_folders.bat"|cut -d"\\"" -f2|cut -d"\\\\" -f1-2|uniq';
-preg_match_all('/^(?P<manufacturer>.*)\\(?P<name>.*)$/muU', trim(`{$cmd}`), $matches);
-`rm -rf tosec;`;
+`wget -q "https://www.tosecdev.org/downloads/category/50-2020-07-29?download=99:tosec-dat-pack-complete-3036-tosec-v2020-07-29" -O tosec.zip;unzip -qq -o -d tosec tosec.zip;rm -f tosec.zip;`;
 $platforms = [];
-foreach ($matches['manufacturer'] as $idx => $manufacturer) {
-	if (!array_key_exists($manufacturer, $platforms))
-		$platforms[$manufacturer] = [];
-	$platforms[$manufacturer][] = $matches['name'][$idx];
+foreach (['TOSEC', 'TOSEC-ISO'] as $type) {
+	preg_match_all('/mkdir "(?P<manufacturer>[^\\\\"]+)\\\\(?P<name>[^\\\\"]+)[\\\\"].*$/muU', file_get_contents('tosec/Scripts/create folders/'.$type.'_folders.bat'), $matches);
+	foreach ($matches['manufacturer'] as $idx => $manufacturer) {
+		if (!array_key_exists($manufacturer, $platforms))
+			$platforms[$manufacturer] = [];
+		if (!in_array($matches['name'][$idx], $platforms[$manufacturer]))
+			$platforms[$manufacturer][] = $matches['name'][$idx];
+	}
 }
-print_r($platforms);
+`rm -rf tosec;`;
+echo json_encode($platforms, JSON_PRETTY_PRINT);
