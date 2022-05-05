d9583246b8f06ece080de5bfaea7e454bb010c76
Joe Huss <detain@interserver.net>
2022-05-05 05:13:26 -0500

    updates getting the fieldslinker populated with our info

---
+++ cron_daily.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+base=/storage/local/ConSolo
+cd ${base}/src/Importing/DAT/TOSEC && php update.php
+cd ${base}/src/Importing/DAT/NoIntro && php update.php
+cd ${base}/src/Importing/DAT/Redump && php update.php
+#cd ${base}/src/Importing/API/TheGamesDB && php update.php
+cd ${base}/src/Importing/Program/LaunchBox && php update.php
+#cd ${base}/src/Importing/Program/MAME && php update.php
