15e9ae5594208e867ae10e7401e762057240a259
Joe Huss <detain@interserver.net>
2022-06-28 09:53:03 -0400

    package update and improved params for xml2array

--- src/xml2array.php
+++ src/xml2array.php
@@ -16,11 +16,11 @@
  * 		$array =  xml2array(file_get_contents('feed.xml', 1, 'attribute'));
  * @link http://www.bin-co.com/php/scripts/xml2array/
  * @param string $contents The XML text
- * @param integer $get_attributes 1 or 0. If this is 1 the function will get the attributes as well as the tag values - this results in a different array structure in the return value.
+ * @param bool $get_attributes If this is true the function will get the attributes as well as the tag values - this results in a different array structure in the return value.
  * @param string $priority Can be 'tag' or 'attribute'. This will change the way the resulting array structure. For 'tag', the tags are given more importance.
  * @return array The parsed XML in an array form. Use print_r() to see the resulting array structure.
  */
-function xml2array($contents, $get_attributes = 1, $priority = 'tag')
+function xml2array($contents, $get_attributes = true, $priority = 'tag')
 {
 	if (!$contents) {
 		return [];
