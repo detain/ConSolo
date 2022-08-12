44f78dfcf153d87f21c9cc7447d4c16e3e4a878a
Joe Huss <detain@interserver.net>
2022-08-12 04:51:45 -0400

    updates to ecc parser improving order of things

--- src/Importing/Program/emudownloadcenter/parse.php
+++ src/Importing/Program/emudownloadcenter/parse.php
@@ -103,42 +103,55 @@ foreach ($ini['LANGUAGE'] as $id => $value)
     $data['languages'][$id] = $value;
 echo "Loading Platform Ids..\n";
 foreach (($ini = loadIni('emuDownloadCenter/ecc_platform_id.ini'))['ECCID'] as $id => $value)
-    $data['platforms'][(string)$id] = ['id' => (string)$id, 'name' => $value, 'category' => null, 'emulators' => [], 'images' => []];
+    $data['platforms'][(string)$id] = ['id' => (string)$id, 'name' => $value, 'category' => null];
 echo "Loading Platform Categories..\n";
 foreach (($ini = loadIni('emuDownloadCenter/ecc_platform_categories.ini'))['ECCID'] as $id => $value)
     $data['platforms'][$id]['category'] = $value;
-echo "Loading Platform <=> emulator matchups..\n";
-foreach ($ini = loadIni('emuDownloadCenter/ecc_platform_emulators.ini') as $id => $value)
-    $data['platforms'][$id]['emulators'] = array_keys($value);
 gitSetup('emuDownloadCenter.wiki');
 gitSetup('emuControlCenter');
 echo "Loading Platforms...\n";
 foreach ($data['platforms'] as $id => $platform) {
     echo "  Platform {$id}....";
-    echo " (images)";
-    foreach (glob('emuDownloadCenter.wiki/images_platform/ecc_'.$id.'_*') as $imagePath)
-        $data['platforms'][$id]['images'][] = $imagePath;
-    foreach (glob('emuControlCenter/ecc-system/images/platform/ecc_'.$id.'_*') as $imagePath)
-        $data['platforms'][$id]['images'][] = $imagePath;
     // import info system user ecc  and iamges
     foreach (['info', 'system', 'user'] as $type) {
         echo ' ('.$type.' ini)';
-        $data['platforms'][$id][$type] = [];
         foreach (glob('emuControlCenter/ecc-system/system/ecc_'.$id.'_'.$type.'.ini') as $imagePath) {
-            if ($type == 'system') {
-                //$ini = loadRegularIni($imagePath);
-                $ini = loadIni($imagePath);
-            } else {
-                $ini = loadQuotedIni($imagePath);
-            }
+            $ini = $type == 'system' ? loadIni($imagePath) : loadQuotedIni($imagePath);
             foreach ($ini as $section => $value) {
                 $section = camelSnake($section);
+                if ($type == 'user' && $section == 'platform')
+                    continue;
+                if ($type == 'info' && $section == 'resources') {
+                    if (isset($value['web']))
+                        $data['platforms'][$id]['web'] = $value['web'] == '' ? [] : explode("\n", $value['web']);
+                    continue;
+                }
+                if ($type == 'info' && $section == 'general') {
+                    //if ($value['type'] != $data['platforms'][$id]['category'])
+                        //echo "\nCategory {$value['type']} != {$data['platforms'][$id]['category']}\n";
+                    unset($value['type']);
+                    foreach ($value as $subKey => $subValue)
+                        $data['platforms'][$id][$subKey] = $subValue;
+                    continue;
+                }
+                if (!array_key_exists($type, $data['platforms'][$id]))
+                    $data['platforms'][$id][$type] = [];
                 $data['platforms'][$id][$type][$section] = $value;
             }
         }
     }
+    echo " (images)";
+    $data['platforms'][$id]['emulators'] = [];
+    $data['platforms'][$id]['images'] = [];
+    foreach (glob('emuDownloadCenter.wiki/images_platform/ecc_'.$id.'_*') as $imagePath)
+        $data['platforms'][$id]['images'][] = $imagePath;
+    foreach (glob('emuControlCenter/ecc-system/images/platform/ecc_'.$id.'_*') as $imagePath)
+        $data['platforms'][$id]['images'][] = $imagePath;
     echo "\n";
 }
+echo "Loading Platform <=> emulator matchups..\n";
+foreach ($ini = loadIni('emuDownloadCenter/ecc_platform_emulators.ini') as $id => $value)
+    $data['platforms'][$id]['emulators'] = array_keys($value);
 echo "Loading Emulators...\n";
 foreach (($ini = loadIni('emuDownloadCenter/ecc_emulators_id.ini'))['EMUID'] as $id => $value)
     $data['emulators'][(string)$id] = ['id' => (string)$id, 'name' => $value];
@@ -178,7 +191,7 @@ foreach ($data['emulators'] as $id => $emulator) {
 }
 echo "Loading misc Images..\n";
 $data['misc_images'] = glob('emuDownloadCenter.wiki/images_misc/*');
-
+echo "Writing JSON...\n";
 $json = json_encode($data, JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE |  JSON_UNESCAPED_SLASHES);
 if ($json === false) {
     echo json_last_error_msg().PHP_EOL;
@@ -186,5 +199,5 @@ if ($json === false) {
 }
 file_put_contents('emucontrolcenter.json', $json);
 echo "Cleaning up repos..\n";
-foreach ($repos as $repo)
-    echo `rm -rf {$repo};`;
+//foreach ($repos as $repo)
+//    echo `rm -rf {$repo};`;
