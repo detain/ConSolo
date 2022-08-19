8e3becf77937b0c039b5d10373be780234c46208
Joe Huss <detain@interserver.net>
2022-08-19 16:11:59 -0400

    package updates

--- src/Importing/Program/MAME/update_db_file_paths_with_new_version.sql
+++ src/Importing/Program/MAME/update_db_file_paths_with_new_version.sql
@@ -1,1 +1,1 @@
-update files set path=replace(path,'MAME 0.243', 'MAME 0.244'), updated=updated where path like '%MAME 0.243%';
+update files set path=replace(path,'MAME 0.244', 'MAME 0.246'), updated=updated where path like '%MAME 0.244%';
