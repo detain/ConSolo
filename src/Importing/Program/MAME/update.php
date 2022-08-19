d7a5d3007689a2bbeda74042bab1f671b2676873
Joe Huss <detain@interserver.net>
2022-08-19 17:20:55 -0400

    updates to the mame scraper adding some cli options and a help screen

--- src/Importing/Program/MAME/update.php
+++ src/Importing/Program/MAME/update.php
@@ -10,6 +10,18 @@ require_once __DIR__.'/../../../bootstrap.php';
 */
 global $db;
 
+if (in_array('-h', $_SERVER['argv'])) {
+	die("Syntax:
+	php ".$_SERVER['argv'][0]." <options>
+
+Options:
+	-h          this screen
+	-f          force update even if already latest version
+	--no-db     skip the db updates/inserts
+	--keep      keep the XML/JSON files when done
+
+");
+}
 $dataDir = '/storage/local/ConSolo/data';
 $configKey = 'mame';
 $row = $db->query("select * from config where field='{$configKey}'");
@@ -67,7 +79,7 @@ $xml = ['software', 'xml'];
 $removeXml = ['port','chip','display','sound','dipswitch','driver','feature','sample','device_ref','input','biosset','configuration','device','softwarelist','disk','slot','ramoption','adjuster', 'sharedfeat'];
 foreach ($xml as $list) {
 	echo "Getting {$list} List   ";
-	//@mkdir($dataDir.'/json/mame/'.$list);
+	@mkdir($dataDir.'/json/mame/'.$list);
 	$jsonFile = $dataDir.'/json/mame/'.$list.'-'.$version.'.json';
 	$fileName = $dataDir.'/xml/mame/'.$list.'-'.$version.'.xml';
 	if (!file_exists($jsonFile)) {
@@ -83,7 +95,7 @@ foreach ($xml as $list) {
 			$array = $array['mame']['machine'];
 		}
 		echo "Writing to JSON {$jsonFile}";
-		//file_put_contents($jsonFile, json_encode($array, JSON_PRETTY_PRINT));
+		file_put_contents($jsonFile, json_encode($array, JSON_PRETTY_PRINT));
 	} else {
 		$array = json_decode(file_get_contents($jsonFile), TRUE);
 	}
@@ -92,66 +104,68 @@ foreach ($xml as $list) {
 		$fileName =  $dataDir.'/json/mame/'.$list.'/'.$data['name'].'.json';
 		echo ' '.$data['name'];
 		$jsonData = json_encode($data, JSON_PRETTY_PRINT);
-		//file_put_contents($fileName, $jsonData);
+		file_put_contents($fileName, $jsonData);
 		$db
 			->insert($list == 'software' ? 'mame_software_platforms' : 'mame')
 			->cols(['doc' => $jsonData])
 			->lowPriority($config['db']['low_priority'])
 			->query();
 	}
-	echo 'Parsing Into DB ';
-	if ($list == 'software') {
-		$games = [];
-		foreach ($array as $idx => $software) {
-			if (isset($software['software']['name']))
-				$software['software'] = [$software['software']];
-			foreach ($software['software'] as $gameIdx => $gameData) {
-				if (isset($gameData['info'])) {
-					if (isset($gameData['info']['serial'])) {
-						$gameData['serial'] = $gameData['info']['serial'];
+	if (!in_array('--no-db', $_SERVER['argv'])) {
+		echo 'Parsing Into DB ';
+		if ($list == 'software') {
+			$games = [];
+			foreach ($array as $idx => $software) {
+				if (isset($software['software']['name']))
+					$software['software'] = [$software['software']];
+				foreach ($software['software'] as $gameIdx => $gameData) {
+					if (isset($gameData['info'])) {
+						if (isset($gameData['info']['serial'])) {
+							$gameData['serial'] = $gameData['info']['serial'];
+						}
+						unset($gameData['info']);
 					}
-					unset($gameData['info']);
-				}
-				$dataArea = false;
-				$partArea = false;
-				if (isset($gameData['part'])) {
-					$partArea = (isset($gameData['part']['name']) ? [$gameData['part']] : $gameData['part']);
-					unset($gameData['part']);
-				}
-				/*if (isset($gameData['sharedfeat'])) {
-					if (isset($gameData['sharedfeat']['compatibility']))
-						$gameData['compatibility'] =  $gameData['sharedfeat']['compatibility'];
-					unset($gameData['sharedfeat']);
-				}*/
-				$gameData['platform'] = $software['name'];
-				$gameData['platform_description'] = $software['description'];
-				foreach ($removeXml as $remove)
-					if (array_key_exists($remove, $gameData))
-						unset($gameData[$remove]);
-				$gameId = $db
-					->insert('mame_software')
-					->cols($gameData)
-					->lowPriority($config['db']['low_priority'])
-					->query();
-				if ($partArea !== false) {
-					foreach ($partArea as $partData) {
-						$dataArea = false;
-						if (isset($partData['dataarea'])) {
-							$dataArea = $partData['dataarea'];
-							if (isset($dataArea['name']))
-								$dataArea = [$dataArea];
-							foreach ($dataArea as $dataPart) {
-								if (isset($dataPart['rom'])) {
-									if (isset($dataPart['rom']['name']))
-										$dataPart['rom'] = [$dataPart['rom']];
-									foreach ($dataPart['rom'] as $rom) {
-										$rom['software_id'] = $gameId;
-										//echo json_encode($rom).PHP_EOL;
-										$db
-											->insert('mame_software_roms')
-											->cols($rom)
-											->lowPriority($config['db']['low_priority'])
-											->query();
+					$dataArea = false;
+					$partArea = false;
+					if (isset($gameData['part'])) {
+						$partArea = (isset($gameData['part']['name']) ? [$gameData['part']] : $gameData['part']);
+						unset($gameData['part']);
+					}
+					/*if (isset($gameData['sharedfeat'])) {
+						if (isset($gameData['sharedfeat']['compatibility']))
+							$gameData['compatibility'] =  $gameData['sharedfeat']['compatibility'];
+						unset($gameData['sharedfeat']);
+					}*/
+					$gameData['platform'] = $software['name'];
+					$gameData['platform_description'] = $software['description'];
+					foreach ($removeXml as $remove)
+						if (array_key_exists($remove, $gameData))
+							unset($gameData[$remove]);
+					$gameId = $db
+						->insert('mame_software')
+						->cols($gameData)
+						->lowPriority($config['db']['low_priority'])
+						->query();
+					if ($partArea !== false) {
+						foreach ($partArea as $partData) {
+							$dataArea = false;
+							if (isset($partData['dataarea'])) {
+								$dataArea = $partData['dataarea'];
+								if (isset($dataArea['name']))
+									$dataArea = [$dataArea];
+								foreach ($dataArea as $dataPart) {
+									if (isset($dataPart['rom'])) {
+										if (isset($dataPart['rom']['name']))
+											$dataPart['rom'] = [$dataPart['rom']];
+										foreach ($dataPart['rom'] as $rom) {
+											$rom['software_id'] = $gameId;
+											//echo json_encode($rom).PHP_EOL;
+											$db
+												->insert('mame_software_roms')
+												->cols($rom)
+												->lowPriority($config['db']['low_priority'])
+												->query();
+										}
 									}
 								}
 							}
@@ -159,40 +173,42 @@ foreach ($xml as $list) {
 					}
 				}
 			}
-		}
-	} elseif ($list == 'xml') {
-		$machines = [];
-		//foreach ($array['mame']['machine'] as $idx => $machine) {
-		foreach ($array as $idx => $machine) {
-			$roms = false;
-			foreach ($removeXml as $remove)
-				if (array_key_exists($remove, $machine))
-					unset($machine[$remove]);
-			if (isset($machine['rom'])) {
-				$roms = $machine['rom'];
-				unset($machine['rom']);
-				if (isset($roms['name']))
-					$roms = [$roms];
-			}
-			$gameId = $db
-				->insert('mame_machines')
-				->cols($machine)
-				->lowPriority($config['db']['low_priority'])
-				->query();
-			if ($roms !== false) {
-				foreach ($roms as $rom) {
-					$rom['machine_id'] = $gameId;
-					$db
-						->insert('mame_machine_roms')
-						->cols($rom)
-						->lowPriority($config['db']['low_priority'])
-						->query();
+		} elseif ($list == 'xml') {
+			$machines = [];
+			//foreach ($array['mame']['machine'] as $idx => $machine) {
+			foreach ($array as $idx => $machine) {
+				$roms = false;
+				foreach ($removeXml as $remove)
+					if (array_key_exists($remove, $machine))
+						unset($machine[$remove]);
+				if (isset($machine['rom'])) {
+					$roms = $machine['rom'];
+					unset($machine['rom']);
+					if (isset($roms['name']))
+						$roms = [$roms];
+				}
+				$gameId = $db
+					->insert('mame_machines')
+					->cols($machine)
+					->lowPriority($config['db']['low_priority'])
+					->query();
+				if ($roms !== false) {
+					foreach ($roms as $rom) {
+						$rom['machine_id'] = $gameId;
+						$db
+							->insert('mame_machine_roms')
+							->cols($rom)
+							->lowPriority($config['db']['low_priority'])
+							->query();
+					}
 				}
 			}
 		}
 	}
-	@unlink($jsonFile);
-	@unlink($fileName);
+	if (!in_array('--keep', $_SERVER['argv'])) {
+		@unlink($jsonFile);
+		@unlink($fileName);
+	}
 	echo "done\n";
 }
 //echo `rm -rf /tmp/update;`;
