b3888c8d88f67f9514b2a7400fb36ce6978f1df1
Joe Huss <detain@interserver.net>
2022-09-01 18:52:59 -0400

    updated emurelation generator refactoring code

---
+++ src/Matching/emurelation.inc.php
@@ -0,0 +1,51 @@
+<?php
+
+function loadSource($fileName) {
+    $sourceId = basename($fileName, '.json');
+    $source = json_decode(file_get_contents($fileName), true);
+    foreach ($source['platforms'] as $platId => $platData) {
+        $names = [];
+        $nameSuffix = [];
+        $nameSuffix[] = $platData['name'];
+        if (isset($platData['shortName'])) {
+            $nameSuffix[] = $platData['shortName'];
+        }
+        if (isset($platData['altNames'])) {
+            foreach ($platData['altNames'] as $altName) {
+                $nameSuffix[] = $altName;
+            }
+        }
+        $company = null;
+        foreach (['', 'company', 'developer', 'manufacturer'] as $prefixField) {
+            if ($prefixField != '' && isset($platData[$prefixField]) && is_null($company)) {
+                $company = $platData[$prefixField];
+            }
+            foreach ($nameSuffix as $suffix) {
+                //print_r($nameSuffix);
+                //echo "platId {$platId} prefixField {$prefixField} : ".(isset($platData[$prefixField]) ? json_encode($platData[$prefixField])." - ".json_encode($suffix) : json_encode($suffix))."\n";
+                $name = strtolower($prefixField != '' && isset($platData[$prefixField]) ? $platData[$prefixField].' '.$suffix : $suffix);
+                if (!in_array($name, $names)) {
+                    $names[] = $name;
+                }
+            }
+        }
+        if (!isset($platData['company']) && !is_null($company)) {
+            $source['platforms'][$platId]['company'] = $company;
+        }
+        if ($sourceId != 'local') {
+            $source['platforms'][$platId]['names'] = $names;
+        }
+    }
+    return [$sourceId, $source];
+}
+
+function loadSources() {
+    echo 'Loading sources..';
+    $sources = [];
+    foreach (glob(__DIR__.'/../../../emurelation/sources/*.json') as $fileName) {
+        list($sourceId, $source) = loadSource($fileName);
+        $sources[$sourceId] = $source['platforms'];
+    }
+    echo 'done'.PHP_EOL;
+    return $sources;
+}
--- src/Matching/update_emurelation_new.php
+++ src/Matching/update_emurelation_new.php
@@ -1,123 +1,26 @@
 <?php
+
 require_once __DIR__.'/../bootstrap.php';
+require_once __DIR__.'/emurelation.inc.php';
 
 /**
 * @var \Workerman\MySQL\Connection
 */
 global $db, $mysqlLinkId;
-$sourceDir = __DIR__.'/../../../emurelation/sources';
-echo 'Loading sources..';
-$sources = [];
-foreach (glob($sourceDir.'/*.json') as $fileName) {
-    $sourceId = basename($fileName, '.json');
-    if ($sourceId == 'local') {
-        continue;
-    }
-    $json = json_decode(file_get_contents($fileName), true);
-    $sources[$sourceId] = $json['platforms'];
-    foreach ($json['platforms'] as $platId => $platData) {
-        $names = [];
-        $nameSuffix = [];
-        $nameSuffix[] = $platData['name'];
-        if (isset($platData['shortName'])) {
-            $nameSuffix[] = $platData['shortName'];
-        }
-        if (isset($platData['altNames'])) {
-            foreach ($platData['altNames'] as $altName) {
-                $nameSuffix[] = $altName;
-            }
-        }
-        $company = null;
-        foreach (['', 'company', 'developer', 'manufacturer'] as $prefixField) {
-            if ($prefixField != '' && isset($platData[$prefixField]) && is_null($company)) {
-                $company = $platData[$prefixField];
-            }
-            foreach ($nameSuffix as $suffix) {
-                //print_r($nameSuffix);
-                //echo "platId {$platId} prefixField {$prefixField} : ".(isset($platData[$prefixField]) ? json_encode($platData[$prefixField])." - ".json_encode($suffix) : json_encode($suffix))."\n";
-                $name = strtolower($prefixField != '' && isset($platData[$prefixField]) ? $platData[$prefixField].' '.$suffix : $suffix);
-                if (!in_array($name, $names)) {
-                    $names[] = $name;
-                }
-            }
-        }
-        if (!isset($platData['company']) && !is_null($company)) {
-            $sources[$sourceId][$platId]['company'] = $company;
-        }
-        $sources[$sourceId][$platId]['names'] = $names;
-    }
-}
-echo 'done'.PHP_EOL;
-$source = [
-    'platforms' => []
-];
-$locals = [
-    __DIR__.'/../../../emurelation/matches/local.json',
-    __DIR__.'/../../../emurelation/platforms.json'
-];
+$sources = loadSources();
+list($sourceId, $source) = loadSource(__DIR__.'/../../../emurelation/local.json');
 $used = [];
 $allNames = [];
-foreach ($locals as $fileName) {
-    $local = json_decode(file_get_contents($fileName), true);
-    foreach ($local as $platform => $platData) {
-        if (isset($platData['tosec'])) {
-            $platData['tosecpix'] = $platData['tosec'];
-            $platData['toseciso'] = $platData['tosec'];
-        }
-        if (!isset($source['platforms'][$platform])) {
-            $source['platforms'][$platform] = [
-                'id' => $platform,
-                'name' => $platform,
-                'matches' => []
-            ];
-        }
-        foreach ($platData as $linkSourceId => $linkPlatforms) {
-            foreach ($linkPlatforms as $linkPlatformId) {
-                if (isset($sources[$linkSourceId])) {
-                    foreach ($sources[$linkSourceId] as $sourcePlatId => $sourcePlatData) {
-                        if (in_array(strtolower($linkPlatformId), $sourcePlatData['names'])) {
-                            //echo "Found link '{$platform}' => {$linkSourceId} {$sourcePlatData['id']} ".(isset($sourcePlatData['company']) ? "'{$sourcePlatData['company']}' " : '')."'{$sourcePlatData['name']}'\n";
-                            if (!isset($source['platforms'][$platform]['matches'][$linkSourceId])) {
-                                $source['platforms'][$platform]['matches'][$linkSourceId] = [];
-                            }
-                            if (!in_array($sourcePlatId, $source['platforms'][$platform]['matches'][$linkSourceId])) {
-                                $source['platforms'][$platform]['matches'][$linkSourceId][] = $sourcePlatId;
-                                if (!isset($used[$linkSourceId])) {
-                                    $used[$linkSourceId] = [];
-                                }
-                                if (!in_array($sourcePlatId, $used[$linkSourceId]))
-                                    $used[$linkSourceId][] = $sourcePlatId;
-                            }
-                        }
-                        if (isset($sourcePlatData['matches'])) {
-                            foreach ($sourcePlatData['matches'] as $matchSource => $matchData) {
-                                foreach ($matchData as $matchPlatform) {
-                                    if (isset($source['platforms'][$platform]['matches'][$matchSource]) && in_array($matchPlatform, $source['platforms'][$platform]['matches'][$matchSource])) {
-                                        if (!in_array($sourcePlatId, $used[$linkSourceId])) {
-                                            $used[$linkSourceId][] = $sourcePlatId;
-                                            if (!isset($source['platforms'][$platform]['matches'][$linkSourceId])) {
-                                                $source['platforms'][$platform]['matches'][$linkSourceId] = [];
-                                            }
-                                            $source['platforms'][$platform]['matches'][$linkSourceId][] = $sourcePlatId;
-                                            echo "Found {$matchSource}:{$matchPlatform} - {$linkSourceId}:{$sourcePlatId}\n";
-                                        }
-                                    }
-
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 foreach ($source['platforms'] as $localPlatId => $localData) {
     $allNames[$localPlatId] = [];
     $allNames[$localPlatId][] = strtolower($localData['name']);
-    foreach ($localData['matches'] as $matchSourceId => $matchData) {
-        foreach ($matchData as $matchPlatId) {
-            foreach ($sources[$matchSourceId][$matchPlatId]['names'] as $name) {
+    foreach ($localData['matches'] as $sourceId => $sourceList) {
+        foreach ($sourceList as $sourcePlatId) {
+            if (!isset($used[$sourceId])) {
+                $used[$sourceId] = [];
+            }
+            $used[$sourceId][] = $sourcePlatId;
+            foreach ($sources[$sourceId][$sourcePlatId]['names'] as $name) {
                 if (!in_array(strtolower($name), $allNames[$localPlatId])) {
                     $allNames[$localPlatId][] = strtolower($name);
                 }
@@ -136,7 +39,7 @@ $table = [];
 $table[] = "| Source | Type | Mapped | Unmapped | Total | Mapped % |";
 $table[] = "|-|-|-|-|-|-|";
 ksort($sources);
-$sourcesList = json_decode(file_get_contents(__DIR__.'/../../../emurelation/sources.json'), true);
+$sourceDefinitions = json_decode(file_get_contents(__DIR__.'/../../../emurelation/sources.json'), true);
 
 foreach ($sources as $sourceId => $sourceData) {
     if (!isset($used[$sourceId])) {
@@ -169,10 +72,10 @@ foreach ($sources as $sourceId => $sourceData) {
     $unmatchedCount = isset($unmatched['platforms'][$sourceId]) ? count($unmatched['platforms'][$sourceId]) : 0;
     $totalCount = $usedCount + $unmatchedCount;
     $usedPct = round($usedCount / $totalCount * 100, 1);
-    if (!isset($sourcesList[$sourceId])) {
+    if (!isset($sourceDefinitions[$sourceId])) {
         echo "Cant find {$sourceId} in sources list\n";
     }
-    $table[] = "| [{$sourcesList[$sourceId]['name']}](sources/{$sourceId}.json) | {$sourcesList[$sourceId]['type']} | {$usedCount} | {$unmatchedCount} | {$totalCount} | {$usedPct}% |";
+    $table[] = "| [{$sourceDefinitions[$sourceId]['name']}](sources/{$sourceId}.json) | {$sourceDefinitions[$sourceId]['type']} | {$usedCount} | {$unmatchedCount} | {$totalCount} | {$usedPct}% |";
 }
 foreach ($unmatched['platforms'] as $key => $values) {
     ksort($values);
@@ -184,3 +87,4 @@ $readme = str_replace($matches['table'][0], implode("\n", $table)."\n", $readme)
 file_put_contents(__DIR__.'/../../../emurelation/README.md', $readme);
 file_put_contents(__DIR__.'/../../../emurelation/unmatched.json', json_encode($unmatched, getJsonOpts()));
 file_put_contents(__DIR__.'/../../../emurelation/local.json', json_encode($source, getJsonOpts()));
+
