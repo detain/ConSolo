239862a49035b3e31a8ac33fc30c7eceaff6daf8
Joe Huss <detain@interserver.net>
2022-10-13 04:33:56 -0400

    updated emucontrolcenter emurelation updater to import the versions

--- src/Matching/update_emurelation_set_emulators_from_emucontrolcenter.php
+++ src/Matching/update_emurelation_set_emulators_from_emucontrolcenter.php
@@ -29,6 +29,36 @@ foreach ($localSource['emulators'] as $localEmuId => $localEmuData) {
                     $localSource['emulators'][$localEmuId][$field] = $sourceEmuData[$field];
                 }
             }
+            if (isset($sourceEmuData['versions'])) {
+                foreach ($sourceEmuData['versions'] as $versionTag => $versionData) {
+                    if (!isset($versionData['type']) || !in_array($versionData['type'], ['Program', 'Installer'])) // skip Extra and unset/null values
+                        continue;
+                    if (preg_match('/-installer$/', $versionTag) && isset($sourceEmuData['versions'][preg_replace('/-installer$/', '', $versionTag)])) // skip if same version non-installer exists
+                        continue;
+                    if ($versionData['category'] != 'Emulator') // skip Driver, Plugin, Tool, and unset/null values
+                        continue;
+                    if (isset($versionData['os']) && in_array($versionData['os'], ['Dos', 'Mac']))
+                        $versionData['os'] = strtoupper($versionData['os']);
+                    if (isset($versionData['os_version']))
+                        $versionData['os_ver'] = str_replace([', XP', ', Vista', 'OSX', 'OS_X', 'OS X'], [',XP', ',Vista', 'OS-X', 'OS-X', 'OS-X'], $versionData['os_version']);
+                    $version = [
+                        'url' => 'https://consolo.is.cc/emu/'.$localEmuId.'/'.$versionTag.'.7z',
+                        'bin' => (isset($versionData['executable_folder']) && $versionData['executable_folder'] != '' ? $versionData['executable_folder'] .'\\' : '').$versionData['executable_file'],
+                        'bit' => isset($versionData['os_architecture']) && $versionData['os_architecture'] == 'x64' ? 64 : 32,
+                    ];
+                    if ($versionData['release_date'] != '20??-??-??') {
+                        $version['date'] = $versionData['release_date'];
+                    }
+                    foreach (['file_notes', 'notes', 'os', 'os_ver'] as $field) {
+                        if (isset($versionData[$field]) && $versionData[$field] != '') {
+                            $version[$field] = $versionData[$field];
+                        }
+                    }
+                    if (!isset($localSource['emulators'][$localEmuId]['versions']))
+                        $localSource['emulators'][$localEmuId]['versions'] = [];
+                    $localSource['emulators'][$localEmuId]['versions'][$versionTag] = $version;
+                }
+            }
             if (isset($sourceEmuData['platforms']) && count($sourceEmuData['platforms']) > 0) {
                 if (!isset($localSource['emulators'][$localEmuId]['platforms'])) {
                     $localSource['emulators'][$localEmuId]['platforms'] = [];
@@ -48,5 +78,5 @@ foreach ($localSource['emulators'] as $localEmuId => $localEmuData) {
         }
     }
 }
-echo count($missing)." Missing Platforms: ".implode(', ', $missing)."\n";
+echo "Missing Platforms: ".json_encode($missing)."\n";
 file_put_contents(__DIR__.'/../../../emurelation/emulators/local.json', json_encode($localSource['emulators'], getJsonOpts()));
