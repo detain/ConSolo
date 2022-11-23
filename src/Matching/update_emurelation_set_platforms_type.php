e6176b33affeb3ecf37fddacb90257c063c73a27
Joe Huss <detain@interserver.net>
2022-11-23 00:54:15 -0500

    work on the platforms type script

--- src/Matching/update_emurelation_set_platforms_type.php
+++ src/Matching/update_emurelation_set_platforms_type.php
@@ -10,26 +10,35 @@ global $db, $mysqlLinkId;
 $sourceMap = [
     'oldcomputers' => 'type',
     'arrm' => 'type',
-    'screenscraper' => 'type',
     'emutopia' => 'type',
     'emucontrolcenter' => 'category',
+    'screenscraper' => 'type',
 ];
-$sources = loadSources(true);
+
 $localSource = loadSourceId('local', true);
+$sources = [];
 $count = 0;
+$totalCount = count($localSource['platforms']);
 foreach ($localSource['platforms'] as $localPlatId => $localPlatData) {
     if (!isset($localPlatData['matches']))
         continue;
     $lastSet = false;
     foreach ($sourceMap as $sourceId => $field) {
+        if (!isset($sources[$sourceId]))
+            $sources[$sourceId] = json_decode(file_get_contents(__DIR__.'/../../../emulation-data/'.$sourceId.'.json'), true);
         if (isset($localPlatData['matches'][$sourceId])) {
-            if (isset($sources[$sourceId]['platforms'][$localPlatData['matches'][$sourceId][0]][$field]) && !is_null($sources[$sourceId]['platforms'][$localPlatData['matches'][$sourceId][0]][$field]))
-                if ($lastSet === false) {
-                    $count++;
-                    $lastSet = true;
-                }
-                $localSource['platforms'][$localPlatId]['type'] = ucwords($sources[$sourceId]['platforms'][$localPlatData['matches'][$sourceId][0]][$field]);
+            $matchId = $localPlatData['matches'][$sourceId][0];
+            $platData = $sources[$sourceId]['platforms'][$matchId];
+            if (!isset($platData[$field]) || is_null($platData[$field]))
+                continue;
+            $value = $platData[$field];
+            if ($lastSet === false) {
+                $count++;
+                $lastSet = true;
+            }
+            $localSource['platforms'][$localPlatId]['type'] = str_replace(['Home Computer', 'Professional Computer', 'Computers'], ['Computer', 'Computer', 'Computer'], ucwords($value));
         }
     }
 }
 file_put_contents(__DIR__.'/../../../emurelation/platforms/local.json', json_encode($localSource['platforms'], getJsonOpts()));
+echo "set the type on {$count}/{$totalCount} platforms\n";
